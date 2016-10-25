aaf08f40be3e3bd4236df0c63d1940a92de30541
Daniel Vattay <wths@Daniel-Vattays-MacBook-Pro.local>
2016-10-25 20:40:14 +0200

    adding exercise 13

---
+++ cpp-basics/pointers/workshop/13.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+// Create a function that prints each word in a string into a new line!
+// Create an other function that takes a string and an array of strings as it's input 
+// and puts each word in the original string into a new element in the array of strings.
+// Decide which argument should be a pointer and which shouldn't.
+// In the main function, print out each element of the array into a new line!
+
+
+int main(int argc, char **argv){
+
+
+	return 0;
+}
+
+
+
