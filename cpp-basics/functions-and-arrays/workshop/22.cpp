00c4bb7263448a96dd6cf88083b9ed0acd84909a
Daniel Vattay <wths@Daniel-Vattays-MacBook-Pro.local>
2016-10-25 14:24:56 +0200

    Addedd exercise 22 to practice return values, signatures and planning functions.

---
+++ cpp-basics/functions-and-arrays/workshop/22.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+//Create a function to tell You whether a certain number is part of a 10 long array of integers.
+//Create an other function to give You the first index of a given integer in an array of integers. 
+//Create an other functions to give You the index of the last occurrence of the given integer in 
+//the array.If the array does not include the given integer this function should return -1.
+//Do not duplicate functionality, use the functions You've created earlier instead of duplicating.
+//Create 5 arrays, each 10 long filled with integers.
+//Find the first and last occurrances of the integer: 14. And Print them out. If the array does not
+//include the integr You should say so in words.
+
+
+int main(int argc, char** argv){
+
+
+	return 0;
+}
