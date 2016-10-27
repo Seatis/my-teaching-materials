b8e023594f26c7a30f42677e83e9144da64e28f0
Tamas Kokeny <tamas.kokeny.87@gmail.com>
2016-10-27 11:11:18 +0200

    feat(cpp-practice): add hangman

---
+++ cpp-basics/practice/workshop/14.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+  // Create a program that playes hangman
+  //
+  // It should have a hardcoded word.
+  // The user should be able to guess wrong five times
+  //
+  // It should request for letters like:
+  // Give me a letter:
+  //
+  // If the word contains the letter:
+  //
+  // As an answer it should print the word in its current form like:
+  // _PP_E
+  // Remaining wrong guesses: 5
+  //
+  // If don't contains:
+  // 
+  // It should reduce the remaing wrong guesses by one, and print the error:
+  //
+  // Wrong guess
+  // Remaining wrong guesses 4
+  //
+  // If the user ran out of guesses, it should display: Game over! :(
+  //
+  // If the user figured out all the letters in the word it should display: You Win! :)
+  // 
+
+  return 0;
+}
