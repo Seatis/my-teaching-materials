c022cb4fc1e71bd97a7e7efa107163170d106bf4
mgyongyosi <mgyongyosi@users.noreply.github.com>
2016-11-01 22:59:38 +0100

    Small change in one of the exercise

--- java-basics/1-installing-basics/workshop/Workshop13.java
+++ java-basics/1-installing-basics/workshop/Workshop13.java
@@ -1,9 +1,8 @@
 public class Workshop13{
   public static void main(String... args){
-    float PI = 3.14159265359;
     int r = 3;
+    // compute and print the area of the circle (radius = 3)
 
-    System.out.println(a/b);
+    System.out.println();
   }
 }
-
