54c461a76f001cf3aa080e209ba94126265e524f
mgyongyosi <mgyongyosi@users.noreply.github.com>
2016-11-07 13:48:52 +0100

    java workshop file fixed

--- java-basics/4-arraylists-foreach/workshop/Workshop15.java
+++ java-basics/4-arraylists-foreach/workshop/Workshop15.java
@@ -2,9 +2,7 @@ import java.util.*;
 
 public class Workshop15{
   public static void main(String... args){
-    ArrayList<Integer> al = new ArrayList<Integer>(Arrays.asList(1, 2, 3, 4, 5, 6, 7 ));
+    ArrayList<String> al = new ArrayList<String>(Arrays.asList("1", "2", "3", "4", "5", "6", "7" ));
     // Add "a" to every string in al. Use a for(each) loop
   }
 }
-
-
