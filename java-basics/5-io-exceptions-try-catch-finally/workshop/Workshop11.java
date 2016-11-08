1b51074a46dcf164ff5b473d83a71737ded3b8c3
EricHofer <eahofer@gmail.com>
2016-11-08 15:04:22 +0100

    .

--- java-basics/5-io-exceptions-try-catch-finally/workshop/Workshop11.java
+++ java-basics/5-io-exceptions-try-catch-finally/workshop/Workshop11.java
@@ -1,12 +1,16 @@
 /**************************************************************************************************
  * Workshop: Practice Exceptions: Throw multiple exceptions
  * 
- * Go back the 7 ate 9 example, and extend to also throw exceptions for other numbers, but 
- * with different exceptions
+ * Go back to the Workshop07 (7 ate 9) and extend it to also throw an exception for 9 (it's no longer
+ * useful as a number, it's just a "pile of bones")
  *
  * see the example at http://beginnersbook.com/2013/12/throws-keyword-example-in-java/
  * to see how one can create multiple throws
  *
+ * INPUT? 7
+ * Exception: 7's are cannibals
+ * INPUT? 9
+ * Exception: Nothing but bones 
  **************************************************************************************************/
 
 public class Workshop11{   
