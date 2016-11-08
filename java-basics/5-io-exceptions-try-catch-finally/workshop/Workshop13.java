347f6751961203a98c11d9b0e1553849b47e8e44
EricHofer <eahofer@gmail.com>
2016-11-08 16:32:19 +0100

    .

--- java-basics/5-io-exceptions-try-catch-finally/workshop/Workshop13.java
+++ java-basics/5-io-exceptions-try-catch-finally/workshop/Workshop13.java
@@ -1,3 +1,26 @@
+/**
+ * File I/O: Scan a Directory
+ * 
+ * There are situations where you'll need to find stuff in a directory.  In this challenge, you're asking the 
+ * user to tell you where to look and handling the error if the user tells you something wrong.
+ * 
+ * What we mentors want to see is a function that will list the contents (files and directories) that are 
+ * beneath a particular path; handle the error if it does not exist.
+ * 
+ * Additionally, say somebody enters a file name down a particular path and that path is right, but the 
+ * file does not exist.  No problem, the person must have made a mistake.  So say, "hey, that file isn't there, 
+ * perhaps you want one of these instead" and then show the contents of the directory.  For example:-
+ * PATH/FILE? 
+ * C:\Temp\Marley.txt
+ * SORRY, No Marley.txt in C:\Temp, here're the files in that directory
+ * Tosh.txt
+ * Wailer.txt
+ * Cliff.txt
+ *
+ * Lastly handle users writing C:\TEMP and C:\TEMP\.  And if there's a file called "TEMP" in the C: drive
+ * show that.
+ */
+
 import java.io.File;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -28,7 +51,7 @@ public class Workshop13 {
         // Implement this function
         // List all files and directories at the specified path
         // If a specified path does not exist write "The specified path does not exists!"
-        // If a specified path is a file and not a directory than list all the contents of the specified file's parent
-        // directory (example: input is c:\myfolder\myfile.txt than the function should list contents of c:\myfolder\
+        // If a specified path is a file and not a directory then list all the contents of the specified file's parent
+        // directory (example: input is c:\myfolder\myfile.txt then the function should list contents of c:\myfolder\
     }
 }
