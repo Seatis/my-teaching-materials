0ab4184e2b679f4a6456c9fe56a237c570eeca66
Daniel Vattay <wths@Daniel-Vattays-MacBook-Pro.local>
2016-11-08 19:42:25 +0100

    clean desk

--- cpp-oo/inheritance/workshop/01.cpp
+++ cpp-oo/inheritance/workshop/01.cpp
@@ -1,17 +1,7 @@
 #include <iostream>
-#include <string>
 
 using namespace std;
 
-class Student {
-  public:
-    string name;
-    unsigned int age;
+class Shape {
+	
 };
-
-int main() {
-  // Create a new instace of the Student class and set it's name to "John" and
-  // it's age to 21
-
-  return 0;
-}
