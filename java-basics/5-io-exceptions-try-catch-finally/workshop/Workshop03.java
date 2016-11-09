d77d342aceac58a4b460c128223ee514e384b745
Eric Hofer <eahofer@gmail.com>
2016-11-09 16:23:27 +0100

    Added loop for reading in the number

--- java-basics/5-io-exceptions-try-catch-finally/workshop/Workshop03.java
+++ java-basics/5-io-exceptions-try-catch-finally/workshop/Workshop03.java
@@ -1,29 +1,43 @@
+** * Created by Verőci Ádám on 2016.11.09..
+ */
 /**************************************************************************************************
- * Workshop: Practice With Exceptions: Arithmetic Overflow
- * Write a routine that catches an arithmetic overflow (that is, larger than the Integer.MAX_VALUE)
- * Note that Integer and int are NOT the same thing; and that Integer offers you properties such as
- * MAX_VALUE.
+ * Workshop: Practice With Exceptions: InputMismatchException
+ * Write a routine that catches an InputMismatchException.
+ *
  * Use the Scanner routine to take the input from the keyboard; if the value is okay then
- * print "Yum", and then 1/2 the value entered.  For example:-
+ * print "Yum", and show the first half of the value entered, for example:-
+ *
  * INPUT or 0 to end?  12334556677723131
- * Invalid value 
+ * Too High
  * (and the Java exception messages)
  * INPUT or 0 to end?  12345678
  * Yum 1234
- * INPUT or 0 to end?  123456
- * Yum 123
+ * INPUT or 0 to end?  12345
+ * Yum 12
  * INPUT or 0 to end? 0
  * Bye!
  **************************************************************************************************/
-
 import java.util.*;
-
 public class Workshop03{
-
-    public static void main(String[] args){
-
-    } //main
-} //Workshop03
-
-
-
+    static Scanner userInput = new Scanner(System.in);
+    public static void main(String[] args) {
+        Integer number = 1;
+        String s ;
+        while (number !=0 ) {
+            number = checkIfValid();
+            s = number.toString();
+            System.out.println("Yum" +  s.substring(0,s.length()/2));
+        }
+    }
+    public static Integer checkIfValid() {
+        int i =0;
+        boolean flLoop = true;
+        while (flLoop) {
+            System.out.print("Type in a number: ");
+            i = userInput.nextInt();
+            flLoop = false;
+            userInput.nextLine();
+        }
+        return i;
+    }
+ } //Workshop03
