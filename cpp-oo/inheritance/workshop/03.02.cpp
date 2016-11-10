a94b516e01a74439db4152ba932ec4a1744cc8a6
Daniel Vattay <wths@Daniel-Vattays-MacBook-Pro.local>
2016-11-10 12:06:02 +0100

    asd

--- cpp-oo/inheritance/workshop/03.02.cpp
+++ cpp-oo/inheritance/workshop/03.02.cpp
@@ -8,6 +8,7 @@ class ParentClass {
 	protected:
 		int mVariable;
 	public:
+		// Please note, that none of these are virtual. This will be important.
 		void setVariable(int _var);
 		ParentClass();
 		int getVariable();
@@ -26,11 +27,16 @@ int ParentClass::getVariable() {
 }
 
 class ChildClass : public ParentClass {
+	// Please note, that here I'm creating variables with the same name in
+	// a descendant class. Which will also inherit the variable of the same
+	// name from it's parent. Possibility of confusion ensues.
 	protected:
 		int mVariable;
 	public:
 		void setVariable(int _var);
 		int getVariable();
+		// These two methods are here to demonstrate how We can reach the 
+		// variable of the same name from the parent class.
 		int getParentVariable();
 		void setParentVariable(int _var);
 };
@@ -53,9 +59,14 @@ void ChildClass::setParentVariable(int _var) {
 
 int main() {
 	ParentClass* a = new ChildClass();
+	cout << "Calling the method of the same name sets two different variables" << endl;
+	cout << "if I call them on the same object, but once I look at it as ParentClass*" << endl;
+	cout << "Once I look at it as ChildClass* after casting it so." << endl; 
 	a->setVariable(2);
 	((ChildClass*)a)->setVariable(4);
+	cout << "Se the resulting variables for Yourself:" << endl;
 	cout << a->getVariable() << "  " << ((ChildClass*)a)->getVariable() << endl;
+	cout << "Now let's see how we can deal with them still by referencing them trough their original calsses namespace:" << endl;
 	((ChildClass*)a)->setParentVariable(8);
 	cout << a->getVariable() << "  " << ((ChildClass*)a)->getVariable() << endl;
 	return 0;
