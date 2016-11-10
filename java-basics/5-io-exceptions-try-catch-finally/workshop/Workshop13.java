a4e626163959f5c0e299bce807cde987e200d817
Eric Hofer <eahofer@gmail.com>
2016-11-10 15:45:31 +0100

    Simplified req to avoid re-opening an object

--- java-basics/5-io-exceptions-try-catch-finally/workshop/Workshop13.java
+++ java-basics/5-io-exceptions-try-catch-finally/workshop/Workshop13.java
@@ -7,18 +7,26 @@
  * What we mentors want to see is a function that will list the contents (files and directories) that are 
  * beneath a particular path; handle the error if it does not exist.
  * 
- * Additionally, say somebody enters a file name down a particular path and that path is right, but the 
- * file does not exist.  No problem, the person must have made a mistake.  So say, "hey, that file isn't there, 
- * perhaps you want one of these instead" and then show the contents of the directory.  For example:-
  * PATH/FILE? 
- * C:\Temp\Marley.txt
- * SORRY, No Marley.txt in C:\Temp, here're the files in that directory
+ * C:\Temppp
+ * SORRY  C:\Tempppp is not a directory or does not exist.
+ * Bye! 
+ 
+ //// Or if the directory exists:
+ * PATH/FILE? 
+ * C:\Temp
  * Tosh.txt
  * Wailer.txt
  * Cliff.txt
- *
- * Lastly handle users writing C:\TEMP and C:\TEMP\.  And if there's a file called "TEMP" in the C: drive
- * show that.
+ * 
+ * Bye!
+ 
+ * Note that you are writing a program that starts and then exits.  You are not doing a loop (as we've not covered 
+ * closing objects).
+ * You are having to create a File (so there'll be a defined object as you saw in the previous Workshop).
+ * Once you have created the File, you need to check if it's a directory
+ * If it's a directory, then you need to set an array of files in the directory 
+ * Then you need a foreach
  */
 
 import java.io.File;
@@ -28,17 +36,16 @@ import java.util.Scanner;
 
 public class Workshop13 {
 
+    //Declare something here (see Workshop12)
+    
     public static void main(String[] args) {
 
-        System.out.println("Please provide an absolute path and I will list the contents of it! (Type 'q' to quit)");
-
         Scanner userInput = new Scanner(System.in);
-
         String pathToList;
 
         System.out.print("Path: ");
 
-        while(userInput.hasNextLine()){
+        if (userInput.hasNextLine()){
             pathToList = userInput.nextLine();
             if(pathToList.equals("q"))
                 break;
@@ -49,9 +56,9 @@ public class Workshop13 {
 
     private static void printDirectoryContents(String pathToList) {
         // Implement this function
-        // List all files and directories at the specified path
-        // If a specified path does not exist write "The specified path does not exists!"
-        // If a specified path is a file and not a directory then list all the contents of the specified file's parent
-        // directory (example: input is c:\myfolder\myfile.txt then the function should list contents of c:\myfolder\
+        // Open the File
+        // Does it exist?
+        // Is it a directory
+        // If so, set an array & loop
     }
 }
