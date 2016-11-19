6efc7425e97207e5c2ba456fb63f8406112e77ef
Tamas Kokeny <tamas.kokeny.87@gmail.com>
2016-11-19 23:19:18 +0100

    feat(cpp-cli-usage): usage feature works

---
+++ exercises/command-line-apps/solutions/cpp/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main() {
+  std::cout << "CLI Todo application" << std::endl;
+  std::cout << "====================" << std::endl;
+  std::cout << std::endl;
+  std::cout << "Command line arguments:" << std::endl;
+  std::cout << "-l   Lists all the tasks" << std::endl;
+  std::cout << "-a   Adds a new task" << std::endl;
+  std::cout << "-r   Removes an task" << std::endl;
+  std::cout << "-c   Completes an task" << std::endl;
+
+
+
+  return 0;
+}
