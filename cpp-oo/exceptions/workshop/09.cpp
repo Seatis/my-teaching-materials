e52fcb8474b8e5f77dc3c608c11f741090610f31
Daniel Vattay <wths@Daniel-Vattays-MacBook-Pro.local>
2016-11-22 10:42:20 +0100

    moved 09 & 10

--- cpp-oo/exceptions/workshop/09.cpp
+++ cpp-oo/exceptions/workshop/09.cpp
@@ -2,11 +2,10 @@
 #include <string>
 using namespace std;
 
-// Create a fixed size stack. The stack can contain only 5 elements.
-// You can push on the stack and pop from the stack.
-// Write the push and pop methods so, that each throws an integer value if there is an error. 
-// Like when the stack is empty or full. Just make sure those are different values.
-// Write a method called string interpretException(int exception) that returns the error message string for the integer.
-//
-// Illustrate in the main function both when it works fine and when exceptions occur.
+// Duplicate the Stack class from the previous exercies.
+// Rewrite it so, that the push and pop methods don't throw integers.
+// Define an enum for te two types of exceptions.
+// Adapt the interpretException method to except an argument from that enum type.
+// Adapt the main function so that the try and catch blocks except an exception of that enum type.
+
 
