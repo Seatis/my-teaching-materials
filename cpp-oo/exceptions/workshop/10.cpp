69909b098961ee6f440d82c5f1cfd39bbb66a681
Daniel Vattay <wths@Daniel-Vattays-MacBook-Pro.local>
2016-11-22 11:57:58 +0100

    Added 10th exercise

---
+++ cpp-oo/exceptions/workshop/10.cpp
@@ -0,0 +1,14 @@
+nclude <iostream>
+#include <string>
+using namespace std;
+
+// Duplicate the Stack class from the previous exercies.
+// Create a class for Your exception.
+// The class should have two private fields:
+// 1.) A filed from Your enum type to represent the kind of error.
+// 2.) A field of type: int. So You can give back the element or the index which caused the problems.
+// Set all the fields in the constructor only.
+// Create get... methods for both variables.
+// Create an std::string toString() method, which gives back a comprehensive error message.
+//
+// Demonstrate how this works in the main function. Print the error messages from the main function.
