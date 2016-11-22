d657b3bf5b554822d1e10575b67d02bb5cafa92f
Daniel Vattay <wths@Daniel-Vattays-MacBook-Pro.local>
2016-11-22 11:39:15 +0100

    upd

--- cpp-oo/exceptions/workshop/05.cpp
+++ cpp-oo/exceptions/workshop/05.cpp
@@ -8,8 +8,7 @@ using namespace std;
 // The character it uses to split should not be in any of the new strings.
 // Example: split("What a nice day", ' ');
 // Result: {"What", "a", "nice",  "day"}
-// If the index is out of bounds, throw an exception.
-// If the character is not in the string throw a different exception.
+// If the character is not in the string throw an exception.
 //
 // In the main function demonstrate the function when working, and when throwing an exception. 
 // Write the results to the console.
