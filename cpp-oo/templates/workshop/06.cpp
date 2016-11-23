647026b5c42c7b30e95df9f53bbe1cebef1174d7
Suviii <istvan.suvoltos@gmail.com>
2016-11-23 11:24:19 +0100

    tasks

---
+++ cpp-oo/templates/workshop/06.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+  // Create a function template that takes an array, and its length and return the sum of his elements
+  // Create a template specialization for char types, that it returns the sum of the characters integer position in the alphabet
+  // Create a template specialization for string types, that it returns the sum of the string's first characters position in the alphabet
+  return 0;
+}
---
+++ cpp-oo/templates/workshop/07.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+	//Create a simple class template which contains 2 item and has both a get and a set method for these!
+  return 0;
+}
