b468c5c180a936ae9e9010c628d189278a3f19fc
Suviii <istvan.suvoltos@gmail.com>
2016-11-23 12:30:57 +0100

    tasks corrections

--- cpp-oo/templates/workshop/06.cpp
+++ cpp-oo/templates/workshop/06.cpp
@@ -4,8 +4,7 @@
 using namespace std;
 
 int main() {
-  // Create a function template that takes an array, and its length and return the sum of his elements
+  // Create a function template that takes an array, and its length and return the sum of it elements
   // Create a template specialization for char types, that it returns the sum of the characters integer position in the alphabet
-  // Create a template specialization for string types, that it returns the sum of the string's first characters position in the alphabet
   return 0;
 }
---
+++ cpp-oo/templates/workshop/08.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+	//Create a class template for a calculator
+        //it should have all the basic methods: add, subtract, multiply, divide
+        //use exceptions to handle errors in special cases! (find the special cases!)
+  return 0;
+}
