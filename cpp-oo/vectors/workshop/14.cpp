75faf0393597522651a31a8a89ec2e7f0b87e78c
Suviii <istvan.suvoltos@gmail.com>
2016-11-24 08:33:25 +0100

    Update vectors-14.cpp

--- cpp-oo/vectors/workshop/14.cpp
+++ cpp-oo/vectors/workshop/14.cpp
@@ -8,16 +8,16 @@ int main() {
 	//"Moszkvics", "Volga", "ZISZ", "Tatra", "Zbrojovka", "Hotchkiss", "DAF", "Warszawa", "Puli", "Balaton", 
 	//"Benz", "Marauder", "Zsiguli" , "IFA", "Fjord", "Trabant", "Wartburg", "Martini"
 	//Create a vector of strings, for the following colors:
-	//"Piros", "Narancssárga", "Zöld", "Kék", "Türkiz", "Barna", "Lila", "Sárga", "Fekete", "Fehér"
+	//"Piros", "NarancssÃ¡rga", "ZÃ¶ld", "KÃ©k", "TÃ¼rkiz", "Barna", "Lila", "SÃ¡rga", "Fekete", "FehÃ©r"
 	//Create a Car class, which holds 2 strings, the name of a car, and a color
 	//Create a 3 dimensional vector of our car class, that will represent a "parking house"
 		//1 dimension: row of cars
 		//2 dimension: cars on a platform like a table game
 		//3 dimension: "parking house"
-	//Make this parking house 4*4*8 (4*4 platform on 8 floors)
+	//Make this parking house 4*4*8 (4*4 platform on 8 floors), then fill it with cars :)
 	//Give these cars a random type from our car type vector
 	//Give these cars a random color from our color vector
-	//Print how much "Sárga"-"Zsiguli" we have, with their position in the parking house!
+	//Print how much "SÃ¡rga"-"Zsiguli" we have, with their position in the parking house!
 	//At the end empty the parking house!
 
   return 0;
