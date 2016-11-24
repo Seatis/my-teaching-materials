9600085909a365a68620efeabc23a64e6756b71c
Suviii <istvan.suvoltos@gmail.com>
2016-11-24 09:55:42 +0100

    update 14.cpp

--- cpp-oo/vectors/workshop/14.cpp
+++ cpp-oo/vectors/workshop/14.cpp
@@ -9,15 +9,17 @@ int main() {
 	//"Benz", "Marauder", "Zsiguli" , "IFA", "Fjord", "Trabant", "Wartburg", "Martini"
 	//Create a vector of strings, for the following colors:
 	//"Piros", "Narancssárga", "Zöld", "Kék", "Türkiz", "Barna", "Lila", "Sárga", "Fekete", "Fehér"
-	//Create a Car class, which holds 2 strings, the name of a car, and a color
+	//Create a Car class, which holds 3 strings, the name of a car, the color of it, and the platenumber in a format like "AAA"-"000"
 	//Create a 3 dimensional vector of our car class, that will represent a "parking house"
 		//1 dimension: row of cars
 		//2 dimension: cars on a platform like a table game
 		//3 dimension: "parking house"
-	//Make this parking house 4*4*8 (4*4 platform on 8 floors), then fill it with cars :)
-	//Give these cars a random type from our car type vector
-	//Give these cars a random color from our color vector
-	//Print how much "Sárga"-"Zsiguli" we have, with their position in the parking house!
+	//Make this parking house 4*4*8 (4*4 platform on 8 floors), then fill it with cars! :)
+	//Give these cars a random type from our car type vector!
+	//Give these cars a random color from our color vector!
+	//Give these cars a random plate number!
+	//Make sure there are no duplicates in car plates!
+	//Print out how much "Sárga"-"Zsiguli" we have, with their position and platenumber in the parking house!
 	//At the end empty the parking house!
 
   return 0;
