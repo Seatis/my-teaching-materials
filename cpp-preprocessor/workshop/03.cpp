a13e5be433cfb60abf05b12616a5bce3e5198e5c
Daniel Vattay <wths@Daniel-Vattays-MacBook-Pro.local>
2016-12-12 12:37:13 +0100

    adding 03.cpp

---
+++ cpp-preprocessor/workshop/03.cpp
@@ -0,0 +1,7 @@
+// Create a constant using preprocessor directives.
+// Make it so, that depending on the existence of a defined variable
+// it's value is different.
+// Write a main function that writes it to the console.
+// Experiment in Your IDE compiling it with the directive defined or not.
+// (This is waht we used -D __DEBUG for example. You can try this and see how it works
+// when You compile a Debug version and a Release version.)
