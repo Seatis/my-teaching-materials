98dd0dc3f6c90e10c0be818faccf7f37fef90cc4
Daniel Vattay <wths@Daniel-Vattays-MacBook-Pro.local>
2016-12-12 12:47:57 +0100

    add 06.cpp, Constant defined and used.

---
+++ cpp-preprocessor/workshop/06.cpp
@@ -0,0 +1,6 @@
+// In Your main function create an array of integers. 5 long.
+// Fill it up with random numbers.
+// For each number treat it as the radius of a circle.
+// Calculate the circumference and the area of the circle with such a radius.
+// Use preprocessor to define the MY_PI constant. Use this in th calculations.
+
