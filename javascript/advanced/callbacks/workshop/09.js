8f646f5fe0f27d6504c7d13cc03504bb4ce764da
Tibor Szász <tszasz@gmail.com>
2016-12-13 11:42:08 +0100

    clarification

--- javascript/advanced/callbacks/workshop/09.js
+++ javascript/advanced/callbacks/workshop/09.js
@@ -1,4 +1,4 @@
 // Create a function called ready
-//  - Embed this script https://api.github.com/orgs/greenfox-academy/repos?callback=ready
+//  - Embed this API endpoint as a <script> tag: https://api.github.com/orgs/greenfox-academy/repos?callback=ready
 //  - create the ready function
 //  - itearate through response.data array and console.log the name property
