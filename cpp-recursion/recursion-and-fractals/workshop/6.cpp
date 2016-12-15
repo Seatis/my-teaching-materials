527ebd7f3828ae0fd8cca93d10979dd5db6a893c
Suviii <istvan.suvoltos@gmail.com>
2016-12-15 11:27:33 +0100

    cpp-recursion-6.cpp

--- cpp-recursion/recursion-and-fractals/workshop/6.cpp
+++ cpp-recursion/recursion-and-fractals/workshop/6.cpp
@@ -4,10 +4,14 @@
 using namespace std;
 
 int main() {
-// We have bunnies standing in a line, numbered 1, 2, ... The odd bunnies
-// (1, 3, ..) have the normal 2 ears. The even bunnies (2, 4, ..) we'll say
-// have 3 ears, because they each have a raised foot. Recursively return the
-// number of "ears" in the bunny line 1, 2, ... n (without loops or
+// We have reindeers standing in a line, numbered 1, 2, ... The odd reindeers
+ 
+// (1, 3, ..) have the normal 2 antlers. The even reindeers (2, 4, ..) we'll say
+ 
+// have 3 antlers, because they each have a raised branch (how funny they are, arent they?). 
+ 
+// Recursively return the number of "antlers" in the reindeer line 1, 2, ... n (without loops or
+ 
 // multiplication).
 
   return 0;
