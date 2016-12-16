9163ef62f82beaa05ee88c1689e78efea943c0cc
Suviii <istvan.suvoltos@gmail.com>
2016-12-16 10:48:49 +0100

    Update 12.cpp

--- cpp-recursion/recursion-and-fractals/workshop/12.cpp
+++ cpp-recursion/recursion-and-fractals/workshop/12.cpp
@@ -6,6 +6,7 @@ using namespace std;
 int main() {
 // write a recursive function that can add numbers in
 // [1, 2, [3, 4], 1, [1, [2, 4]]]
+// remember the usage of class vectors! :)
 
   return 0;
 }
