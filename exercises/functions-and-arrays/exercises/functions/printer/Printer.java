e7af96982be404d39858e85d377e9cf1bc41f91f
Fejes Aniko <oranjje@gmail.com>
2017-03-21 12:15:28 +0100

    feat(functions-and-arrays): create the class wrapper on your own, java

--- exercises/functions-and-arrays/exercises/functions/printer/Printer.java
+++ exercises/functions-and-arrays/exercises/functions/printer/Printer.java
@@ -1,3 +1,6 @@
+//  Create the usual class wrapper
+//  and main method on your own.
+
 // - Create a function called `printer`
 //   which prints the input String parameters
 //   (can have multiple number of arguments)
