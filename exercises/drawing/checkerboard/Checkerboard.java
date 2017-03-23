64e4cb518270ada01953dffbe2d428cf8a27d508
Fejes Aniko <oranjje@gmail.com>
2017-03-23 14:59:02 +0100

    feat(drawing): boilerplating java exercises, round 1

--- exercises/drawing/checkerboard/Checkerboard.java
+++ exercises/drawing/checkerboard/Checkerboard.java
@@ -1,2 +1,34 @@
-// create a 300x300 canvas.
-// fill it with a checkerboard pattern.
+import javax.swing.*;
+
+import java.awt.*;
+
+import static javax.swing.JFrame.EXIT_ON_CLOSE;
+
+public class Drawing {
+
+    public static void mainDraw(Graphics graphics){
+      // fill the canvas with a checkerboard pattern.
+
+
+
+    }
+
+//    Don't touch the code below
+    public static void main(String[] args) {
+        JFrame jFrame = new JFrame("Drawing");
+        jFrame.setSize(new Dimension(300, 300));
+        jFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        jFrame.add(new ImagePanel());
+        jFrame.setLocationRelativeTo(null);
+        jFrame.setVisible(true);
+    }
+    static class ImagePanel extends JPanel{
+        @Override
+        protected void paintComponent(Graphics graphics) {
+            super.paintComponent(graphics);
+            mainDraw(graphics);
+
+        }
+    }
+
+}
--- exercises/drawing/line-play/LinePlay.java
+++ exercises/drawing/line-play/LinePlay.java
@@ -1,2 +1,35 @@
-// reproduce this:
-// [https://github.com/greenfox-velox/velox-syllabus/blob/master/week-04/3-graphics/workshop/r1.png]
+import javax.swing.*;
+
+import java.awt.*;
+
+import static javax.swing.JFrame.EXIT_ON_CLOSE;
+
+public class Drawing {
+
+    public static void mainDraw(Graphics graphics){
+      // reproduce this:
+      // [https://github.com/greenfox-velox/velox-syllabus/blob/master/week-04/3-graphics/workshop/r1.png]
+
+
+
+    }
+
+//    Don't touch the code below
+    public static void main(String[] args) {
+        JFrame jFrame = new JFrame("Drawing");
+        jFrame.setSize(new Dimension(300, 300));
+        jFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        jFrame.add(new ImagePanel());
+        jFrame.setLocationRelativeTo(null);
+        jFrame.setVisible(true);
+    }
+    static class ImagePanel extends JPanel{
+        @Override
+        protected void paintComponent(Graphics graphics) {
+            super.paintComponent(graphics);
+            mainDraw(graphics);
+
+        }
+    }
+
+}
