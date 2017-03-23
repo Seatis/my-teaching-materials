6bf042c66bd960cc5720c487cb531140b99ef88b
Fejes Aniko <oranjje@gmail.com>
2017-03-23 15:04:28 +0100

    feat(drawing): boilerplating java exercises, round 2

--- exercises/drawing/position-square/PositionSquare.java
+++ exercises/drawing/position-square/PositionSquare.java
@@ -1,5 +1,37 @@
-// create a 300x300 canvas.
-// create a square drawing function that takes 2 parameters:
-// the x and y coordinates of the square's top left corner
-// and draws a 50x50 square from that point.
-// draw 3 squares with that function.
+import javax.swing.*;
+
+import java.awt.*;
+
+import static javax.swing.JFrame.EXIT_ON_CLOSE;
+
+public class Drawing {
+
+    public static void mainDraw(Graphics graphics){
+      // create a square drawing function that takes 2 parameters:
+      // the x and y coordinates of the square's top left corner
+      // and draws a 50x50 square from that point.
+      // draw 3 squares with that function.
+
+
+
+    }
+
+//    Don't touch the code below
+    public static void main(String[] args) {
+        JFrame jFrame = new JFrame("Drawing");
+        jFrame.setSize(new Dimension(300, 300));
+        jFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        jFrame.add(new ImagePanel());
+        jFrame.setLocationRelativeTo(null);
+        jFrame.setVisible(true);
+    }
+    static class ImagePanel extends JPanel{
+        @Override
+        protected void paintComponent(Graphics graphics) {
+            super.paintComponent(graphics);
+            mainDraw(graphics);
+
+        }
+    }
+
+}
