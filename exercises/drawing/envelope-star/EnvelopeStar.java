3eecd04b0440b3c54db157beb7327aa534d65f64
Fejes Aniko <oranjje@gmail.com>
2017-03-23 14:42:16 +0100

    feat(drawing): raw copy exercise 9-16

---
+++ exercises/drawing/envelope-star/EnvelopeStar.java
@@ -0,0 +1,2 @@
+// reproduce this:
+// [https://github.com/greenfox-velox/velox-syllabus/blob/master/week-04/3-graphics/workshop/r2.png]
---
+++ exercises/drawing/line-play-quarters/LinePlayQuarters.java
@@ -0,0 +1,3 @@
+// divide the canvas into 4 parts
+// and repeat this pattern in each quarter:
+// [https://github.com/greenfox-velox/velox-syllabus/blob/master/week-04/3-graphics/workshop/r1.png]
