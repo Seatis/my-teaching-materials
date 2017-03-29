41cd3b4750cfbff41c74c4a44df30045f887a14b
Levente Belteky <belteky.levente@gmail.com>
2017-03-29 19:44:21 +0200

    refactor(oo): make dominoes follow the style guide for python

--- exercises/oo/dominoes/python/domino.py
+++ exercises/oo/dominoes/python/domino.py
@@ -1,6 +1,6 @@
-class Domino:
-  def __init__(self, value_a, value_b):
-    self.values = [value_a, value_b]
+class Domino(object):
+    def __init__(self, value_a, value_b):
+        self.values = [value_a, value_b]
 
-  def __repr__(self):
-    return '[{}, {}]'.format(self.values[0], self.values[1])
+    def __repr__(self):
+        return '[{}, {}]'.format(self.values[0], self.values[1])
--- exercises/oo/dominoes/python/dominoes.py
+++ exercises/oo/dominoes/python/dominoes.py
@@ -1,14 +1,14 @@
 from domino import Domino
 
 def initialize_dominoes():
-  dominoes = []
-  dominoes.append(Domino(5, 2))
-  dominoes.append(Domino(4, 6))
-  dominoes.append(Domino(1, 5))
-  dominoes.append(Domino(6, 7))
-  dominoes.append(Domino(2 ,4))
-  dominoes.append(Domino(7, 1))
-  return dominoes
+    dominoes = []
+    dominoes.append(Domino(5, 2))
+    dominoes.append(Domino(4, 6))
+    dominoes.append(Domino(1, 5))
+    dominoes.append(Domino(6, 7))
+    dominoes.append(Domino(2 ,4))
+    dominoes.append(Domino(7, 1))
+    return dominoes
 
 dominoes = initialize_dominoes()
 # You have the list of Dominoes
