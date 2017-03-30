d950f35ac93286f985c51bd4eff51f104c3521e9
matheb <mathebogi@gmail.com>
2017-03-30 06:41:03 +0200

    fix(dice): dice.py corrected

--- exercises/oo/dice/python/dice.py
+++ exercises/oo/dice/python/dice.py
@@ -3,24 +3,22 @@ import random
 class Dice():
 
     def __init__(self):
-        self.dices = [0, 0, 0, 0, 0, 0]
-        self.current = None
+        self.dice = [0, 0, 0, 0, 0, 0]
 
     def roll(self):
-        for i in range(len(self.dices)):
-            self.dices[i] = random.randint(1,6)
-        return self.dices
+        for i in range(len(self.dice)):
+            self.dice[i] = random.randint(1,6)
+        return self.dice
 
     def getCurrent(self, index=None):
         if index != None:
-            print('The current state of dice['+str(index)+']: ', self.dices[self.current])
+            print('The current state of dice['+str(index)+']: ', self.dice[index])
         else:
-            print('The current state of dices: ', self.dices)
+            print('The current state of dice: ', self.dice)
 
     def reroll(self, index=None):
-        self.current = index
         if index != None:
-            self.dices[index-1] = random.randint(1,6)
+            self.dice[index] = random.randint(1,6)
         else:
             self.roll()
-        return self.dices
+        return self.dice
