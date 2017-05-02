0a162d3ac1ae840e6e9ed154bc41f76f68efd5e7
Szász Tibor <tszasz@gmail.com>
2017-05-02 17:28:20 +0200

    feat(workshop): python data sructures are converted to javascript

---
+++ workshop/data-structures/lists/isinlist/isinlist.js
@@ -0,0 +1,6 @@
+'use strict';
+// Check if array contains all of the following elements: 4,8,12,16
+// Create a 'numChecker' function that accepts 'listOfNumbers' as an input
+// it should return "true" if it contains all, otherwise "false"
+
+var listOfNumbers = [2, 4, 6, 8, 10, 12, 14, 16]
---
+++ workshop/data-structures/lists/quoteswap/quoteswap.js
@@ -0,0 +1,8 @@
+'use strict';
+// Accidentally I messed up this quote from Richard Feynman.
+// Two words are out of place
+// Your task is to fix it by swapping the right words with code
+
+// Also, log the sentence to the console with spaces in between.
+
+var words = ["What", "I", "do", "create,", "I", "cannot", "not", "understand."];
