e880b5bd191376a24a04a22fb8ae5be1fdd22fa8
Gyorgy Gabor <gyorgyg@gmail.com>
2017-05-07 23:44:09 +0200

    feat(adv-js-01): add exercises to the workshop

---
+++ workshop/advanced-js-01/workshop/variables01.js
@@ -0,0 +1,12 @@
+// Ferenc Vágási is a good working man. He helps for his friend Góliát for carrying furniture.
+// Ferenc's wife Jutka wants to know where is his husband. She calls the company. They tell her Feri is finished today he is drinking a beer at the next kocsma.
+// Make the code true. Góliát is working now.
+
+const isWorking = true;
+const worker = 'Feri';
+
+if (isWorking) {
+  let worker = 'Góliát';
+}
+
+console.log('Who is working now?', worker);
