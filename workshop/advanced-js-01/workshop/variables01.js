c1df2a6b968500060834804a724fdbaa359b9461
Gábor György <gyorgyg@gmail.com>
2017-05-07 23:46:23 +0200

    fix(adv-js-01) fix line break

--- workshop/advanced-js-01/workshop/variables01.js
+++ workshop/advanced-js-01/workshop/variables01.js
@@ -1,5 +1,6 @@
 // Ferenc Vágási is a good working man. He helps for his friend Góliát for carrying furniture.
-// Ferenc's wife Jutka wants to know where is his husband. She calls the company. They tell her Feri is finished today he is drinking a beer at the next kocsma.
+// Ferenc's wife Jutka wants to know where is his husband. She calls the company. 
+// They tell her Feri is finished today he is drinking a beer at the next kocsma.
 // Make the code true. Góliát is working now.
 
 const isWorking = true;
