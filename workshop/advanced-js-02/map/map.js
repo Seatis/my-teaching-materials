4cdf9a91a9a2ce730eb62ed9ae59c05ed6cbe996
Tamas Kokeny <tamas.kokeny.87@gmail.com>
2017-05-10 01:32:38 +0200

    feat(advanced-js-02): add exercises

---
+++ workshop/advanced-js-02/map/map.js
@@ -0,0 +1,15 @@
+'use strict';
+
+var fruits = [
+  'melon',
+  'apple',
+  'strawberry',
+  'blueberry',
+  'pear',
+  'banana'
+];
+
+// Create a new array of consists numbers that shows how many times the 'e' letter
+// occurs in the word stored under the same index at the fruits array!
+// Please use the map method.
+
