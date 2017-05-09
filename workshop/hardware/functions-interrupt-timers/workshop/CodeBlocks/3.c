52e0e4900d9805187fd1ee297402752673ec58f5
leventekobor94 <levente.kobor94@gmail.com>
2017-05-09 15:50:18 +0200

    Update 3.c

--- workshop/hardware/functions-interrupt-timers/workshop/CodeBlocks/3.c
+++ workshop/hardware/functions-interrupt-timers/workshop/CodeBlocks/3.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /* Write a non void function which returns the value of PI, so the following number:
-3.1415926535897932384626433832795028841971693993
+3.14159
 
 Try it out in the main function by printing out the result of the pi generator function!
 */
