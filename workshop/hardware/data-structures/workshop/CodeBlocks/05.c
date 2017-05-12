b3fdaf1bdd5f7bb084f6c24e5cf9757c8850cef3
koincidencia <kazykazy91@gmail.com>
2017-05-12 18:56:13 +0200

    feat(hw-data-structures): add exercises

---
+++ workshop/hardware/data-structures/workshop/CodeBlocks/05.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+struct Pirate {
+    char name[256];
+    int has_wooden_leg;
+    short int gold_count;
+};
+
+// TODO:
+// Create a function that takes an array of pirates (and it's length) then returns the sum of the golds of all pirates
+
+// TODO:
+// Create a function that takes an array of pirates (and it's length) then returns the average of the gold / pirate
+
+// TODO:
+// Create a function that takes an array of pirates (and it's length) then returns the name of the
+// richest that has wooden leg
+
+int main() {
+    struct Pirate priates[] = {
+        {"Jack", 0, 18},
+        {"Uwe", 1, 8},
+        {"Hook", 1, 12},
+        {"Halloween kid", 0, 0},
+        {"Sea Wolf", 1, 14},
+        {"Morgan", 0, 1}
+    };
+
+    return 0;
+}
---
+++ workshop/hardware/data-structures/workshop/CodeBlocks/08.c
@@ -0,0 +1,23 @@
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+// Create a struct called IntBuffer
+// It should store: 
+// A pointer that points to a dynamically allocated array of ints (could be null)
+// The length of the array
+
+// Create a function that returns a dynamically allocated IntBuffer (the buffer itself should be allocated as well, not just the array inside)
+// It should take a normal array and it's length as argument, and it should fill all the buffer values from the array
+
+// Create a function that resizes an IntBuffer, it should take an IntBuffer as reference and change that buffers length and reallocate it's array as well. (copying it's values)
+
+// Create a function that takes an IntBuffer and returns a new IntBuffer that only contains the the even numbers from the old intBuffer
+
+// Try all the functions in the main function, plase deallocate any buffer and the array inside it after using them
+
+int main() {
+
+  return 0;
+}
