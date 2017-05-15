87a7638b3532f9b37bf099dbb76f559db5137615
morpheus-PC\morpheus <sunikovacs@gmail.com>
2017-05-15 19:49:09 +0200

    feat(data-structures-dyn-memory): add solutions

---
+++ workshop/hardware/solutions/data-structures-dyn-memory/CodeBlocks/DataStructures/01.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdint.h>
+
+struct Computer {
+    float cpu_speed_GHz;
+    int ram_size_GB;
+    int bits;
+};
+
+int main() {
+    struct Computer computer = {3.2, 8, 32};
+
+    //TODO: Change the bits to 64
+
+    //TODO: print out the structure members
+
+    return 0;
+}
---
+++ workshop/hardware/solutions/data-structures-dyn-memory/CodeBlocks/DataStructures/02.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+struct RectangularCuboid {
+    double a;
+    double b;
+    double c;
+};
+
+// TODO:
+// Write a function called "GetSurface" that takes a RectangularCuboid
+// and returns it's surface
+int GetSurface(struct RectangularCuboid rect_cub)
+{
+    return (2*rect_cub.a*rect_cub.b + 2*rect_cub.a*rect_cub.c + 2*rect_cub.c*rect_cub.b);
+}
+
+
+// TODO:
+// Write a function called "GetVolume" that takes a RectangularCuboid
+// and returns it's volume
+int GetVolume (struct RectangularCuboid rect_cub)
+{
+    return (rect_cub.a*rect_cub.b*rect_cub.c);
+}
+
+int main() {
+
+    struct RectangularCuboid rect = {2, 2, 2};
+    printf("Surface: %d\nVolume: %d", GetSurface(rect), GetVolume(rect));
+    return 0;
+}
