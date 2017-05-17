c5b2f012ba29741b1434772ef321fe12cefd46c6
koincidencia <kazykazy91@gmail.com>
2017-05-17 20:27:46 +0200

    feat(hw-EEPROM-solutions): add EEPROM_basics

--- workshop/hardware/solutions/state-machine-EEPROM/EEPROM_basics/EEPROM_basics/main.c
+++ workshop/hardware/solutions/state-machine-EEPROM/EEPROM_basics/EEPROM_basics/main.c
@@ -1,12 +1,22 @@
 #include <avr/io.h>
 #include <avr/eeprom.h>
 #include <stdint.h>
+#include <stdio.h>
 #include "UART_driver.h"
 
 // TODO:
-// Comment out the unnecessary functionality
-#define READ_FROM_EEPROM
-#define WRITE_DEFAULTS_TO_EEPROM
+/*
+	1) leave everything uncommented! Write the other TODOs first, then run the program.
+		On the serial port you can see the default values of the variables
+	2) now uncomment the "READ_FROM_EEPROM" definition. This will enable EEPROM read at startup.
+		you will see random numbers on UART, because you just read uninitialized data from the EEPROM
+	3) now comment out the READ_FROM_EEPROM definition, and uncomment the other one
+		On the serial port you can see the default values of the variables
+	4) now uncomment the "READ_FROM_EEPROM" definition, and comment out the other one
+		you will see the defaults numbers, which were loaded from the EEPROM
+*/
+//#define READ_FROM_EEPROM
+//#define WRITE_DEFAULTS_TO_EEPROM
 
 uint8_t var_8bit = 8;
 uint16_t var_16bit = 16;
@@ -27,11 +37,21 @@ void Init() {
     UART_Init();
 
 #ifdef WRITE_DEFAULTS_TO_EEPROM
-    // TODO: write the default values to the EEPROM
+    // TODO: write the default values of the variables to the EEPROM
+    eeprom_update_byte(VAR_8BIT_ADDR, var_8bit);
+    eeprom_update_word(VAR_16BIT_ADDR, var_16bit);
+    eeprom_update_dword(VAR_32BIT_ADDR, var_32bit);
+    eeprom_update_float(VAR_FLOAT_ADDR, var_float);
+    eeprom_update_block(array, ARRAY_ADDR, 6);
 #endif
 
 #ifdef READ_FROM_EEPROM
-    // TODO: read the values from the EEPROM
+    // TODO: read the values from the EEPROM to the variables
+    var_8bit = eeprom_read_byte(VAR_8BIT_ADDR);
+    var_16bit = eeprom_read_word(VAR_16BIT_ADDR);
+    var_32bit = eeprom_read_dword(VAR_32BIT_ADDR);
+    var_float = eeprom_read_float(VAR_FLOAT_ADDR);
+    eeprom_read_block(array, ARRAY_ADDR, 6);
 #endif
 }
 
@@ -50,6 +70,8 @@ int main(void) {
     //----- END OF STDIO IO BUFFER SETUP
 
     // Print out the variables
+    // printf can't print out floating point numbers (float) by deafault :(
+    // Just don't care, it's okay
     printf("var_8bit\t%d\r\n", var_8bit);
     printf("var_16bit\t%d\r\n", var_16bit);
     printf("var_32bit\t%d\r\n", var_32bit);
