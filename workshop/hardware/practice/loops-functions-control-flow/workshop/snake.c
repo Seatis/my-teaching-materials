15324774c72bf62d2b25d21452018278327eb2dc
koincidencia <kazykazy91@gmail.com>
2017-05-21 20:02:27 +0200

    feat(hw-practice-exercises): add tasks

---
+++ workshop/hardware/practice/loops-functions-control-flow/workshop/snake.c
@@ -0,0 +1,13 @@
+/*
+Try to write a console program which is similar to the classig game "snake".
+
+You will need consol related special library functions.
+On windows use conio.h
+On linux use curses.h
+
+You can use the getch() function to get the code of the button which was pressed
+by the user.
+
+At first try to make a snake which goes straight on the screen.
+After that try to change the direction of the movement with the arrow keys.
+*/
