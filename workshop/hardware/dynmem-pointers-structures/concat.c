ee4bc74490582731e049c06f0a1dbfcdca7c6407
morpheus-PC\morpheus <sunikovacs@gmail.com>
2017-05-21 18:41:05 +0200

    feat(hw-dynmem-pointers-structures-samu) add tasks

---
+++ workshop/hardware/dynmem-pointers-structures/concat.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+//TODO: Implement a function that concatenates 2 strings (dest and source). The return value should be the concatenated strings.
+
+int main() {
+
+    return 0;
+}
---
+++ workshop/hardware/dynmem-pointers-structures/under_avarage.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// TODO:
+// Create a function that takes a double array as a parameter, and the length of array
+// this function should calculate the avarage of the elements
+// calculate how much elements are under the avarage
+// allocate a dynamic array, and load into it, the elements that are under the avarage
+// the return value should be the length of the new array
+
+int under_avarage(double* array, int length, double* new_array);
+
+int main() {
+
+    return 0;
+}
