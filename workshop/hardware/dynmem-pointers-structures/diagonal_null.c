8130b358dc7c4daf7d6d21b93ca991d6f5eb2317
samsmith94 <sunikovacs@gmail.com>
2017-05-21 19:59:19 +0200

    Update diagonal_null.c

--- workshop/hardware/dynmem-pointers-structures/diagonal_null.c
+++ workshop/hardware/dynmem-pointers-structures/diagonal_null.c
@@ -3,9 +3,10 @@
 
 // TODO:
 // Write a function that takes a regular integer matrix as parameter (n*n matrix). The other parameter should be the n.
-// The function should return 1 if in the diagonal line of the matrix is full of 0, else return 0.								
-// So if we have a matrix like this:	[0][2]	the return value is 1.
-//										[5][0]
+// The function should return 1 if in the diagonal line of the matrix is full of 0, else return 0.
+
+//[0][2]
+//[5][0] so if we have a matrix like this the return value is 1.
 
 
 int main() {
