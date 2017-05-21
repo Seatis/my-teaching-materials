a7cdd03e358cbc17897fd336e32a4fd0ddc577ae
samsmith94 <sunikovacs@gmail.com>
2017-05-21 20:25:05 +0200

    Update concat.c

--- workshop/hardware/dynmem-pointers-structures/concat.c
+++ workshop/hardware/dynmem-pointers-structures/concat.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-//TODO: Implement a function that concatenates 2 strings (dest and source). The return value should be the concatenated strings.
+//TODO: Implement a function that concatenates 2 strings (dest and source). The return value should be the concatenated string.
 
 int main() {
 
