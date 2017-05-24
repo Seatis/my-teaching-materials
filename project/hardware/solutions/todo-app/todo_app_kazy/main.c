19cb9cd8e5735a1967b54f601c207f0ae7523273
koincidencia <kazykazy91@gmail.com>
2017-05-24 12:08:58 +0200

    feat(hw-todo-app-solution): add read parser

--- project/hardware/solutions/todo-app/todo_app_kazy/main.c
+++ project/hardware/solutions/todo-app/todo_app_kazy/main.c
@@ -6,6 +6,7 @@
 #define COMMAND_LIST_TODOS "-l"
 #define COMMAND_ADD_TODOS "-a"
 #define COMMAND_WRITE_TODOS "-wr"
+#define COMMAND_READ_TODOS "-rd"
 
 int main()
 {
@@ -46,6 +47,19 @@ int main()
 
             // Save to file
             write_todo(&storage, filename);
+
+        } else if (strstr(command, COMMAND_READ_TODOS) != NULL) {
+            char filename[256];
+            // Get parameter
+            char *p;
+            p = strtok(command, "\"");  // Get the substring before the first "
+            p = strtok(NULL, "\"");     // Get the second substring (between the two ")
+
+            // Copy the filename
+            strcpy(filename, p);
+
+            // Save to file
+            read_todo(&storage, filename);
 
         }
 	}
