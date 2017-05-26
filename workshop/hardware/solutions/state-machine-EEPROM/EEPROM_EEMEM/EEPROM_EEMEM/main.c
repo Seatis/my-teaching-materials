4dc6e2d14b669fb8fd398449068523f2023c053f
koincidencia <kazykazy91@gmail.com>
2017-05-26 13:08:26 +0200

    refactor(hw-eeprom): style

--- workshop/hardware/solutions/state-machine-EEPROM/EEPROM_EEMEM/EEPROM_EEMEM/main.c
+++ workshop/hardware/solutions/state-machine-EEPROM/EEPROM_EEMEM/EEPROM_EEMEM/main.c
@@ -44,58 +44,59 @@ uint8_t EEMEM EEPROM_var_8bit = 128;
 uint16_t EEMEM EEPROM_var_16bit = 123;
 uint32_t EEMEM EEPROM_var_32bit = 321;
 float EEMEM EEPROM_var_float = 3.14;
-uint8_t EEMEM EEPROM_array[] = {1,2,3,4,5,6};
+uint8_t EEMEM EEPROM_array[] = {1, 2, 3, 4, 5, 6};
 
-void Init() {
-    // Initialize the UART interface with 115200 baud/sec
-    UART_Init();
+void init()
+{
+	// Initialize the UART interface with 115200 baud/sec
+	UART_init();
 }
 
-int main(void) {
-    // Don't forget to call the Init() function :)
-    Init();
+int main(void)
+{
+	// Don't forget to call the init() function :)
+	init();
 
-    // Setting up STDIO input and output buffer
-    // You don't have to understand this!
-    //----- START OF STDIO IO BUFFER SETUP
-    FILE UART_output = FDEV_SETUP_STREAM(UART_SendCharacter, NULL, _FDEV_SETUP_WRITE);
-    stdout = &UART_output;
-    FILE UART_input = FDEV_SETUP_STREAM(NULL, UART_GetCharacter, _FDEV_SETUP_READ);
-    stdin = &UART_input;
-    //----- END OF STDIO IO BUFFER SETUP
+	// Setting up STDIO input and output buffer
+	// You don't have to understand this!
+	//----- START OF STDIO IO BUFFER SETUP
+	FILE UART_output = FDEV_SETUP_STREAM(UART_send_character, NULL, _FDEV_SETUP_WRITE);
+	stdout = &UART_output;
+	FILE UART_input = FDEV_SETUP_STREAM(NULL, UART_get_character, _FDEV_SETUP_READ);
+	stdin = &UART_input;
+	//----- END OF STDIO IO BUFFER SETUP
 
-    // Create variables in the SRAM
-    uint8_t var_8bit;
-    uint16_t var_16bit;
-    uint32_t var_32bit;
-    float var_float;
-    uint8_t array[6];
+	// Create variables in the SRAM
+	uint8_t var_8bit;
+	uint16_t var_16bit;
+	uint32_t var_32bit;
+	float var_float;
+	uint8_t array[6];
 
-    // TODO:
-    // Load the values from the EEPROM to the SRAM variables!
-    // Use the eeprom_read_xxxxx functions with th EEMEM variables as pointers.
-    var_8bit = eeprom_read_byte(&EEPROM_var_8bit);
-    var_16bit = eeprom_read_word(&EEPROM_var_16bit);
-    var_32bit = eeprom_read_dword(&EEPROM_var_32bit);
-    var_float = eeprom_read_float(&EEPROM_var_float);
-    eeprom_read_block(array, EEPROM_array, sizeof(array));
+	// TODO:
+	// Load the values from the EEPROM to the SRAM variables!
+	// Use the eeprom_read_xxxxx functions with th EEMEM variables as pointers.
+	var_8bit = eeprom_read_byte(&EEPROM_var_8bit);
+	var_16bit = eeprom_read_word(&EEPROM_var_16bit);
+	var_32bit = eeprom_read_dword(&EEPROM_var_32bit);
+	var_float = eeprom_read_float(&EEPROM_var_float);
+	eeprom_read_block(array, EEPROM_array, sizeof(array));
 
-    // Print out the variables
-    // printf can't print out floating point numbers (float) by deafault :(
-    // Just don't care, it's okay
-    printf("var_8bit\t%d\r\n", var_8bit);
-    printf("var_16bit\t%d\r\n", var_16bit);
-    printf("var_32bit\t%ld\r\n", var_32bit);
-    printf("var_float\t%f\r\n", var_float);
-    printf("array\t");
-    for(uint8_t i = 0; i < sizeof(array); i++) {
-        printf("%d ", array[i]);
-    }
-    printf("\r\n");
+	// Print out the variables
+	// printf can't print out floating point numbers (float) by deafault :(
+	// Just don't care, it's okay
+	printf("var_8bit\t%d\r\n", var_8bit);
+	printf("var_16bit\t%d\r\n", var_16bit);
+	printf("var_32bit\t%ld\r\n", var_32bit);
+	printf("var_float\t%f\r\n", var_float);
+	printf("array\t");
+	for (uint8_t i = 0; i < sizeof(array); i++)
+		printf("%d ", array[i]);
 
-    // Infinite loop
-    while (1) {
+	printf("\r\n");
 
-    }
+	// Infinite loop
+	while (1) {
+
+	}
 }
-
