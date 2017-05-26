4c18ae0e4006db0929011f1071d0bd968186e6d8
lollypopp <->
2017-05-26 13:44:32 +0200

    feat(hw-solution-I2C-communication): add solution to I2c communication

---
+++ workshop/hardware/solutions/I2C-communication/TC74_DRIVER/TC74_driver.h
@@ -0,0 +1,19 @@
+// Multiple inclusion guard
+#ifndef _TC74_DRIVER_H
+#define _TC74_DRIVER_H
+
+#include <stdint.h>
+
+#define TC_WRITE	0
+#define TC_READ		1
+#define TC_ADDRESS	0b1001000	//TC74A0 address
+
+uint8_t read_temp (uint8_t address);
+void TWIInit(void);
+void TWIStart(void);
+void TWIWrite(uint8_t u8data);
+void TWIStop(void);
+uint8_t TWIReadNACK(void);
+uint8_t TWIReadACK(void);
+
+#endif // _TC74_DRIVER_H
---
+++ workshop/hardware/solutions/I2C-communication/TC74_DRIVER/main.c
@@ -0,0 +1,59 @@
+#include "TC74_driver.h"
+#include "UART_driver.h"
+#include <avr/io.h>
+#include <stdio.h>
+#include <avr/interrupt.h>
+
+#ifndef F_CPU
+#define F_CPU 16000000UL
+#endif
+#include <avr/delay.h>
+
+#define LED_DDR			DDRB
+#define LED_DDR_POS		DDRB5
+#define LED_PIN			PINB
+#define LED_PIN_POS		PINB5
+#define LED_PORT		PORTB
+#define LED_PORT_POS	PORTB5
+
+void SystemInit() {
+	//TODO
+	// Call the TWI driver init function
+	TWIInit();
+	//TODO
+	//Init the uart
+	UART_Init();
+}
+
+int main(void) {
+
+	// Don't forget to call the init function :)
+	SystemInit();
+
+	// Setting up STDIO input and output buffer
+	// You don't have to understand this!
+	//----- START OF STDIO IO BUFFER SETUP
+	FILE UART_output = FDEV_SETUP_STREAM(UART_SendCharacter, NULL, _FDEV_SETUP_WRITE);
+	stdout = &UART_output;
+	FILE UART_input = FDEV_SETUP_STREAM(NULL, UART_GetCharacter, _FDEV_SETUP_READ);
+	stdin = &UART_input;
+	//----- END OF STDIO IO BUFFER SETUP
+
+	// Try printf
+	printf("Startup...\r\n");
+
+	sei();
+
+	// Infinite loop
+	while (1) {
+		//TODO
+		//Write the temperature frequently.
+		//TODO
+		//Advanced: Don't use delay, use timer.
+		_delay_ms(1000);
+		printf("temp: %d [°C]\r\n",read_temp(TC_ADDRESS));
+		//TODO
+		//Blink the led to make sure the code is running
+		PINB |= 1 <<LED_PORT_POS;
+	}
+}
