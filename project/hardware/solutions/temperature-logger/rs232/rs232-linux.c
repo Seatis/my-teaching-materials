cb8fcbe4381d6b084d88fe8a5ff8eabb0bdf4a89
koincidencia <kazykazy91@gmail.com>
2017-06-01 17:15:18 +0200

    fix(hw-temp-log-solution): style

--- project/hardware/solutions/temperature-logger/rs232/rs232-linux.c
+++ project/hardware/solutions/temperature-logger/rs232/rs232-linux.c
@@ -28,7 +28,7 @@
     LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
     OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
     SOFTWARE.
-  
+
 */
 
 #if defined(__unix__) || defined(__unix) || \
@@ -52,21 +52,21 @@
 /*****************************************************************************/
 /** Base name for COM devices */
 #if defined(__APPLE__) && defined(__MACH__)
-    static const char * devBases[] = {
-        "tty."
-    };
-    static int noBases = 1;
+static const char *devBases[] = {
+	"tty."
+};
+static int noBases = 1;
 #else
-    static const char * devBases[] = {
-        "ttyACM", "ttyUSB", "rfcomm", "ttyS"
-    };
-    static int noBases = 4;
+static const char *devBases[] = {
+	"ttyACM", "ttyUSB", "rfcomm", "ttyS"
+};
+static int noBases = 4;
 #endif
 
 /*****************************************************************************/
 typedef struct {
-    char * port;
-    int handle;
+	char *port;
+	int handle;
 } COMDevice;
 
 #define COM_MAXDEVICES        64
@@ -75,187 +75,223 @@ static int noDevices = 0;
 
 /*****************************************************************************/
 /** Private functions */
-void _AppendDevices(const char * base);
+void _AppendDevices(const char *base);
 int _BaudFlag(int BaudRate);
 
 /*****************************************************************************/
 int comEnumerate()
 {
-    for (int i = 0; i < noDevices; i++) {
-        if (comDevices[i].port) free(comDevices[i].port);
-        comDevices[i].port = NULL;
-    }
-    noDevices = 0;
-    for (int i = 0; i < noBases; i++)
-        _AppendDevices(devBases[i]);
-    return noDevices;
+	for (int i = 0; i < noDevices; i++) {
+		if (comDevices[i].port) free(comDevices[i].port);
+		comDevices[i].port = NULL;
+	}
+	noDevices = 0;
+	for (int i = 0; i < noBases; i++)
+		_AppendDevices(devBases[i]);
+	return noDevices;
 }
 
 void comTerminate()
 {
-    comCloseAll();
-    for (int i = 0; i < noDevices; i++) {
-        if (comDevices[i].port) free(comDevices[i].port);
-        comDevices[i].port = NULL;
-    }
+	comCloseAll();
+	for (int i = 0; i < noDevices; i++) {
+		if (comDevices[i].port) free(comDevices[i].port);
+		comDevices[i].port = NULL;
+	}
 }
 
 int comGetNoPorts()
 {
-    return noDevices;
+	return noDevices;
 }
 
 /*****************************************************************************/
-int comFindPort(const char * name)
+int comFindPort(const char *name)
 {
-    int p;
-    for (p = 0; p < noDevices; p++)
-        if (strcmp(name, comDevices[p].port) == 0)
-            return p;
-    return -1;
+	int p;
+	for (p = 0; p < noDevices; p++)
+		if (strcmp(name, comDevices[p].port) == 0)
+			return p;
+	return -1;
 }
 
-const char * comGetInternalName(int index)
+const char *comGetInternalName(int index)
 {
-    #define COM_MAXNAME    128
-    static char name[COM_MAXNAME];
-    if (index >= noDevices || index < 0)
-        return NULL;
-    sprintf(name, "/dev/%s", comDevices[index].port);
-    return name;
+#define COM_MAXNAME    128
+	static char name[COM_MAXNAME];
+	if (index >= noDevices || index < 0)
+		return NULL;
+	sprintf(name, "/dev/%s", comDevices[index].port);
+	return name;
 }
 
-const char * comGetPortName(int index) {
-    if (index >= noDevices || index < 0)
-        return NULL;
-    return comDevices[index].port;
+const char *comGetPortName(int index)
+{
+	if (index >= noDevices || index < 0)
+		return NULL;
+	return comDevices[index].port;
 }
 
 /*****************************************************************************/
 int comOpen(int index, int baudrate)
 {
-    if (index >= noDevices || index < 0)
-        return 0;
+	if (index >= noDevices || index < 0)
+		return 0;
 // Close if already open
-    COMDevice * com = &comDevices[index];
-    if (com->handle >= 0) comClose(index);
+	COMDevice *com = &comDevices[index];
+	if (com->handle >= 0) comClose(index);
 // Open port
-    printf("Try %s \n", comGetInternalName(index));
-    int handle = open(comGetInternalName(index), O_RDWR | O_NOCTTY | O_NDELAY);
-    if (handle < 0)
-        return 0;
-    printf("Open %s \n", comGetInternalName(index));
+	printf("Try %s \n", comGetInternalName(index));
+	int handle = open(comGetInternalName(index), O_RDWR | O_NOCTTY | O_NDELAY);
+	if (handle < 0)
+		return 0;
+	printf("Open %s \n", comGetInternalName(index));
 // General configuration
-    struct termios config;
-    memset(&config, 0, sizeof(config));
-    tcgetattr(handle, &config);
-    config.c_iflag &= ~(INLCR | ICRNL);
-    config.c_iflag |= IGNPAR | IGNBRK;
-    config.c_oflag &= ~(OPOST | ONLCR | OCRNL);
-    config.c_cflag &= ~(PARENB | PARODD | CSTOPB | CSIZE | CRTSCTS);
-    config.c_cflag |= CLOCAL | CREAD | CS8;
-    config.c_lflag &= ~(ICANON | ISIG | ECHO);
-    int flag = _BaudFlag(baudrate);
-    cfsetospeed(&config, flag);
-    cfsetispeed(&config, flag);
+	struct termios config;
+	memset(&config, 0, sizeof(config));
+	tcgetattr(handle, &config);
+	config.c_iflag &= ~(INLCR | ICRNL);
+	config.c_iflag |= IGNPAR | IGNBRK;
+	config.c_oflag &= ~(OPOST | ONLCR | OCRNL);
+	config.c_cflag &= ~(PARENB | PARODD | CSTOPB | CSIZE | CRTSCTS);
+	config.c_cflag |= CLOCAL | CREAD | CS8;
+	config.c_lflag &= ~(ICANON | ISIG | ECHO);
+	int flag = _BaudFlag(baudrate);
+	cfsetospeed(&config, flag);
+	cfsetispeed(&config, flag);
 // Timeouts configuration
-    config.c_cc[VTIME] = 1;
-    config.c_cc[VMIN]  = 0;
-    //fcntl(handle, F_SETFL, FNDELAY);
+	config.c_cc[VTIME] = 1;
+	config.c_cc[VMIN]  = 0;
+	//fcntl(handle, F_SETFL, FNDELAY);
 // Validate configuration
-    if (tcsetattr(handle, TCSANOW, &config) < 0) {
-        close(handle);
-        return 0;
-    }
-    com->handle = handle;
-    return 1;
+	if (tcsetattr(handle, TCSANOW, &config) < 0) {
+		close(handle);
+		return 0;
+	}
+	com->handle = handle;
+	return 1;
 }
 
 void comClose(int index)
 {
-    if (index >= noDevices || index < 0)
-        return;
-    COMDevice * com = &comDevices[index];
-    if (com->handle < 0) 
-        return;
-    tcdrain(com->handle);
-    close(com->handle);
-    com->handle = -1;
+	if (index >= noDevices || index < 0)
+		return;
+	COMDevice *com = &comDevices[index];
+	if (com->handle < 0)
+		return;
+	tcdrain(com->handle);
+	close(com->handle);
+	com->handle = -1;
 }
 
 void comCloseAll()
 {
-    for (int i = 0; i < noDevices; i++)
-        comClose(i);
+	for (int i = 0; i < noDevices; i++)
+		comClose(i);
 }
 
 /*****************************************************************************/
-int comWrite(int index, const char * buffer, size_t len)
+int comWrite(int index, const char *buffer, size_t len)
 {
-    if (index >= noDevices || index < 0)
-        return 0;
-    if (comDevices[index].handle <= 0)
-        return 0;
-    int res = write(comDevices[index].handle, buffer, len);
-    if (res < 0)
-        res = 0;
-    return res;
+	if (index >= noDevices || index < 0)
+		return 0;
+	if (comDevices[index].handle <= 0)
+		return 0;
+	int res = write(comDevices[index].handle, buffer, len);
+	if (res < 0)
+		res = 0;
+	return res;
 }
 
-int comRead(int index, char * buffer, size_t len)
+int comRead(int index, char *buffer, size_t len)
 {
-    if (index >= noDevices || index < 0)
-        return 0;
-    if (comDevices[index].handle <= 0)
-        return 0;
-    int res = read(comDevices[index].handle, buffer, len);
-    if (res < 0)
-        res = 0;
-    return res;
+	if (index >= noDevices || index < 0)
+		return 0;
+	if (comDevices[index].handle <= 0)
+		return 0;
+	int res = read(comDevices[index].handle, buffer, len);
+	if (res < 0)
+		res = 0;
+	return res;
 }
 
 /*****************************************************************************/
 int _BaudFlag(int BaudRate)
 {
-    switch(BaudRate)
-    {
-        case 50:      return B50; break;
-        case 110:     return B110; break;
-        case 134:     return B134; break;
-        case 150:     return B150; break;
-        case 200:     return B200; break;
-        case 300:     return B300; break;
-        case 600:     return B600; break;
-        case 1200:    return B1200; break;
-        case 1800:    return B1800; break;
-        case 2400:    return B2400; break;
-        case 4800:    return B4800; break;
-        case 9600:    return B9600; break;
-        case 19200:   return B19200; break;
-        case 38400:   return B38400; break;
-        case 57600:   return B57600; break;
-        case 115200:  return B115200; break;
-        case 230400:  return B230400; break;
-        default : return B0; break;
-    }
+	switch (BaudRate) {
+	case 50:
+		return B50;
+		break;
+	case 110:
+		return B110;
+		break;
+	case 134:
+		return B134;
+		break;
+	case 150:
+		return B150;
+		break;
+	case 200:
+		return B200;
+		break;
+	case 300:
+		return B300;
+		break;
+	case 600:
+		return B600;
+		break;
+	case 1200:
+		return B1200;
+		break;
+	case 1800:
+		return B1800;
+		break;
+	case 2400:
+		return B2400;
+		break;
+	case 4800:
+		return B4800;
+		break;
+	case 9600:
+		return B9600;
+		break;
+	case 19200:
+		return B19200;
+		break;
+	case 38400:
+		return B38400;
+		break;
+	case 57600:
+		return B57600;
+		break;
+	case 115200:
+		return B115200;
+		break;
+	case 230400:
+		return B230400;
+		break;
+	default :
+		return B0;
+		break;
+	}
 }
 
-void _AppendDevices(const char * base)
+void _AppendDevices(const char *base)
 {
-    int baseLen = strlen(base);
-    struct dirent * dp;
+	int baseLen = strlen(base);
+	struct dirent *dp;
 // Enumerate devices
-    DIR * dirp = opendir("/dev");
-    while ((dp = readdir(dirp)) && noDevices < COM_MAXDEVICES) {
-        if (strlen(dp->d_name) >= baseLen) {
-            if (memcmp(base, dp->d_name, baseLen) == 0) {
-                COMDevice * com = &comDevices[noDevices ++];
-                com->port = (char *) strdup(dp->d_name);
-                com->handle = -1;
-            }
-        }
-    }
-    closedir(dirp);
+	DIR *dirp = opendir("/dev");
+	while ((dp = readdir(dirp)) && noDevices < COM_MAXDEVICES) {
+		if (strlen(dp->d_name) >= baseLen) {
+			if (memcmp(base, dp->d_name, baseLen) == 0) {
+				COMDevice *com = &comDevices[noDevices ++];
+				com->port = (char *) strdup(dp->d_name);
+				com->handle = -1;
+			}
+		}
+	}
+	closedir(dirp);
 }
 
 #endif // unix
--- project/hardware/solutions/temperature-logger/rs232/rs232-win.c
+++ project/hardware/solutions/temperature-logger/rs232/rs232-win.c
@@ -3,7 +3,7 @@
     Version 0.21, 11/10/2015
     -> WIN32 implementation
     -> rs232-win.c
-    
+
     The MIT License (MIT)
 
     Copyright (c) 2013-2015 Frédéric Meslin, Florent Touchard
@@ -43,8 +43,8 @@ typedef int        bool;
 #define false   0
 
 typedef struct {
-    int port;
-    void * handle;
+	int port;
+	void *handle;
 } COMDevice;
 
 /*****************************************************************************/
@@ -53,50 +53,50 @@ static COMDevice comDevices[COM_MAXDEVICES];
 static int noDevices = 0;
 
 #define COM_MINDEVNAME 16384
-const char * comPtn = "COM???";
+const char *comPtn = "COM???";
 
 /*****************************************************************************/
-const char * findPattern(const char * string, const char * pattern, int * value);
-const char * portInternalName(int index);
+const char *findPattern(const char *string, const char *pattern, int *value);
+const char *portInternalName(int index);
 
 /*****************************************************************************/
 typedef struct _COMMTIMEOUTS {
-    uint32_t ReadIntervalTimeout;
-    uint32_t ReadTotalTimeoutMultiplier;
-    uint32_t ReadTotalTimeoutConstant;
-    uint32_t WriteTotalTimeoutMultiplier;
-    uint32_t WriteTotalTimeoutConstant;
+	uint32_t ReadIntervalTimeout;
+	uint32_t ReadTotalTimeoutMultiplier;
+	uint32_t ReadTotalTimeoutConstant;
+	uint32_t WriteTotalTimeoutMultiplier;
+	uint32_t WriteTotalTimeoutConstant;
 } COMMTIMEOUTS;
 
 typedef struct _DCB {
-    uint32_t DCBlength;
-    uint32_t BaudRate;
-    uint32_t fBinary  :1;
-    uint32_t fParity  :1;
-    uint32_t fOutxCtsFlow  :1;
-    uint32_t fOutxDsrFlow  :1;
-    uint32_t fDtrControl  :2;
-    uint32_t fDsrSensitivity  :1;
-    uint32_t fTXContinueOnXoff  :1;
-    uint32_t fOutX  :1;
-    uint32_t fInX  :1;
-    uint32_t fErrorChar  :1;
-    uint32_t fNull  :1;
-    uint32_t fRtsControl  :2;
-    uint32_t fAbortOnError  :1;
-    uint32_t fDummy2  :17;
-    uint16_t wReserved;
-    uint16_t XonLim;
-    uint16_t XoffLim;
-    uint8_t  ByteSize;
-    uint8_t  Parity;
-    uint8_t  StopBits;
-    int8_t  XonChar;
-    int8_t  XoffChar;
-    int8_t  ErrorChar;
-    int8_t  EofChar;
-    int8_t  EvtChar;
-    uint16_t wReserved1;
+	uint32_t DCBlength;
+	uint32_t BaudRate;
+	uint32_t fBinary  : 1;
+	uint32_t fParity  : 1;
+	uint32_t fOutxCtsFlow  : 1;
+	uint32_t fOutxDsrFlow  : 1;
+	uint32_t fDtrControl  : 2;
+	uint32_t fDsrSensitivity  : 1;
+	uint32_t fTXContinueOnXoff  : 1;
+	uint32_t fOutX  : 1;
+	uint32_t fInX  : 1;
+	uint32_t fErrorChar  : 1;
+	uint32_t fNull  : 1;
+	uint32_t fRtsControl  : 2;
+	uint32_t fAbortOnError  : 1;
+	uint32_t fDummy2  : 17;
+	uint16_t wReserved;
+	uint16_t XonLim;
+	uint16_t XoffLim;
+	uint8_t  ByteSize;
+	uint8_t  Parity;
+	uint8_t  StopBits;
+	int8_t  XonChar;
+	int8_t  XoffChar;
+	int8_t  ErrorChar;
+	int8_t  EofChar;
+	int8_t  EvtChar;
+	uint16_t wReserved1;
 } DCB;
 
 /*****************************************************************************/
@@ -110,213 +110,213 @@ typedef struct _DCB {
 
 /*****************************************************************************/
 /** Windows system functions */
-void * __stdcall CreateFileA(const char * lpFileName, uint32_t dwDesiredAccess, uint32_t dwShareMode, void * lpSecurityAttributes, uint32_t dwCreationDisposition, uint32_t dwFlagsAndAttributes, void * hTemplateFile);
-bool __stdcall WriteFile(void * hFile, const void * lpBuffer, uint32_t nNumberOfBytesToWrite, uint32_t * lpNumberOfBytesWritten, void * lpOverlapped);
-bool __stdcall ReadFile(void * hFile, void * lpBuffer, uint32_t nNumberOfBytesToRead, uint32_t * lpNumberOfBytesRead, void * lpOverlapped);
-bool __stdcall CloseHandle(void * hFile);
+void *__stdcall CreateFileA(const char *lpFileName, uint32_t dwDesiredAccess, uint32_t dwShareMode, void *lpSecurityAttributes, uint32_t dwCreationDisposition, uint32_t dwFlagsAndAttributes, void *hTemplateFile);
+bool __stdcall WriteFile(void *hFile, const void *lpBuffer, uint32_t nNumberOfBytesToWrite, uint32_t *lpNumberOfBytesWritten, void *lpOverlapped);
+bool __stdcall ReadFile(void *hFile, void *lpBuffer, uint32_t nNumberOfBytesToRead, uint32_t *lpNumberOfBytesRead, void *lpOverlapped);
+bool __stdcall CloseHandle(void *hFile);
 
 uint32_t __stdcall GetLastError(void);
 void __stdcall SetLastError(uint32_t dwErrCode);
 
-uint32_t  __stdcall QueryDosDeviceA(const char * lpDeviceName, char * lpTargetPath, uint32_t ucchMax);
+uint32_t  __stdcall QueryDosDeviceA(const char *lpDeviceName, char *lpTargetPath, uint32_t ucchMax);
 
-bool __stdcall GetCommState(void * hFile, DCB * lpDCB);
-bool __stdcall GetCommTimeouts(void * hFile, COMMTIMEOUTS * lpCommTimeouts);
-bool __stdcall SetCommState(void * hFile, DCB * lpDCB);
-bool __stdcall SetCommTimeouts(void * hFile, COMMTIMEOUTS * lpCommTimeouts);
-bool __stdcall SetupComm(void * hFile, uint32_t dwInQueue, uint32_t dwOutQueue);
+bool __stdcall GetCommState(void *hFile, DCB *lpDCB);
+bool __stdcall GetCommTimeouts(void *hFile, COMMTIMEOUTS *lpCommTimeouts);
+bool __stdcall SetCommState(void *hFile, DCB *lpDCB);
+bool __stdcall SetCommTimeouts(void *hFile, COMMTIMEOUTS *lpCommTimeouts);
+bool __stdcall SetupComm(void *hFile, uint32_t dwInQueue, uint32_t dwOutQueue);
 
 /*****************************************************************************/
 int comEnumerate()
 {
 // Get devices information text
-    size_t size = COM_MINDEVNAME;
-    char * list = (char *) malloc(size);
-    SetLastError(0);
-    QueryDosDeviceA(NULL, list, size);
-    while (GetLastError() == ERROR_INSUFFICIENT_BUFFER) {
-        size *= 2;
-        char * nlist = realloc(list, size);
-        if (!nlist) {
-            free(list);
-            return 0;
-        }
-        list = nlist;
-        SetLastError(0);
-        QueryDosDeviceA(NULL, list, size);
-    }
+	size_t size = COM_MINDEVNAME;
+	char *list = (char *) malloc(size);
+	SetLastError(0);
+	QueryDosDeviceA(NULL, list, size);
+	while (GetLastError() == ERROR_INSUFFICIENT_BUFFER) {
+		size *= 2;
+		char *nlist = realloc(list, size);
+		if (!nlist) {
+			free(list);
+			return 0;
+		}
+		list = nlist;
+		SetLastError(0);
+		QueryDosDeviceA(NULL, list, size);
+	}
 // Gather all COM ports
-    int port;
-    const char * nlist = findPattern(list, comPtn, &port);
-    noDevices = 0;
-    while(port > 0 && noDevices < COM_MAXDEVICES) {
-        COMDevice * com = &comDevices[noDevices ++];
-        com->port = port;
-        com->handle    = 0;
-        nlist = findPattern(nlist, comPtn, &port);
-    }
-    free(list);
-    return noDevices;
+	int port;
+	const char *nlist = findPattern(list, comPtn, &port);
+	noDevices = 0;
+	while (port > 0 && noDevices < COM_MAXDEVICES) {
+		COMDevice *com = &comDevices[noDevices ++];
+		com->port = port;
+		com->handle    = 0;
+		nlist = findPattern(nlist, comPtn, &port);
+	}
+	free(list);
+	return noDevices;
 }
 
 void comTerminate()
 {
-    comCloseAll();    
+	comCloseAll();
 }
 
 int comGetNoPorts()
 {
-    return noDevices;
+	return noDevices;
 }
 
 /*****************************************************************************/
-const char * comGetPortName(int index)
+const char *comGetPortName(int index)
 {
-    #define COM_MAXNAME    32
-    static char name[COM_MAXNAME];
-    if (index < 0 || index >= noDevices)
-        return 0;
-    sprintf(name, "COM%i", comDevices[index].port);
-    return name;
+#define COM_MAXNAME    32
+	static char name[COM_MAXNAME];
+	if (index < 0 || index >= noDevices)
+		return 0;
+	sprintf(name, "COM%i", comDevices[index].port);
+	return name;
 }
 
-int comFindPort(const char * name)
+int comFindPort(const char *name)
 {
-    for (int i = 0; i < noDevices; i++)
-        if (strcmp(name, comGetPortName(i)) == 0)
-            return i;
-    return -1;
+	for (int i = 0; i < noDevices; i++)
+		if (strcmp(name, comGetPortName(i)) == 0)
+			return i;
+	return -1;
 }
 
-const char * comGetInternalName(int index)
+const char *comGetInternalName(int index)
 {
-    #define COM_MAXNAME    32
-    static char name[COM_MAXNAME];
-    if (index < 0 || index >= noDevices)
-        return 0;
-    sprintf(name, "//./COM%i", comDevices[index].port);
-    return name;
+#define COM_MAXNAME    32
+	static char name[COM_MAXNAME];
+	if (index < 0 || index >= noDevices)
+		return 0;
+	sprintf(name, "//./COM%i", comDevices[index].port);
+	return name;
 }
 
 /*****************************************************************************/
 int comOpen(int index, int baudrate)
 {
-    DCB config;
-    COMMTIMEOUTS timeouts;
-    if (index < 0 || index >= noDevices) 
-        return 0;
+	DCB config;
+	COMMTIMEOUTS timeouts;
+	if (index < 0 || index >= noDevices)
+		return 0;
 // Close if already open
-    COMDevice * com = &comDevices[index];
-    if (com->handle) comClose(index);
+	COMDevice *com = &comDevices[index];
+	if (com->handle) comClose(index);
 // Open COM port
-    void * handle = CreateFileA(comGetInternalName(index), GENERIC_READ | GENERIC_WRITE, 0, NULL, OPEN_EXISTING, 0, NULL);
-    if (handle == INVALID_HANDLE_VALUE) 
-        return 0;
-    com->handle = handle;
+	void *handle = CreateFileA(comGetInternalName(index), GENERIC_READ | GENERIC_WRITE, 0, NULL, OPEN_EXISTING, 0, NULL);
+	if (handle == INVALID_HANDLE_VALUE)
+		return 0;
+	com->handle = handle;
 // Prepare read / write timeouts
-    SetupComm(handle, 64, 64);
-    timeouts.ReadIntervalTimeout = MAX_DWORD;
-    timeouts.ReadTotalTimeoutConstant = 0;
-    timeouts.WriteTotalTimeoutConstant = 0;
-    timeouts.ReadTotalTimeoutMultiplier = 0;
-    timeouts.WriteTotalTimeoutMultiplier = 0;
-    SetCommTimeouts(handle, &timeouts);
+	SetupComm(handle, 64, 64);
+	timeouts.ReadIntervalTimeout = MAX_DWORD;
+	timeouts.ReadTotalTimeoutConstant = 0;
+	timeouts.WriteTotalTimeoutConstant = 0;
+	timeouts.ReadTotalTimeoutMultiplier = 0;
+	timeouts.WriteTotalTimeoutMultiplier = 0;
+	SetCommTimeouts(handle, &timeouts);
 // Prepare serial communication format
-    GetCommState(handle, &config);
-    config.BaudRate = baudrate;
-    config.fBinary = true;
-    config.fParity = 0;
-    config.fErrorChar = 0;
-    config.fNull = 0;
-    config.fAbortOnError = 0;
-    config.ByteSize = 8;
-    config.Parity = 0;
-    config.StopBits = 0;
-    config.EvtChar = '\n';
+	GetCommState(handle, &config);
+	config.BaudRate = baudrate;
+	config.fBinary = true;
+	config.fParity = 0;
+	config.fErrorChar = 0;
+	config.fNull = 0;
+	config.fAbortOnError = 0;
+	config.ByteSize = 8;
+	config.Parity = 0;
+	config.StopBits = 0;
+	config.EvtChar = '\n';
 // Set the port state
-    if (SetCommState(handle, &config) == 0) {
-        CloseHandle(handle);
-        return 0;
-    }
-    return 1;
+	if (SetCommState(handle, &config) == 0) {
+		CloseHandle(handle);
+		return 0;
+	}
+	return 1;
 }
 
 void comClose(int index)
 {
-    if (index < 0 || index >= noDevices) 
-        return;
-    COMDevice * com = &comDevices[index];
-    if (!com->handle) 
-        return;
-    CloseHandle(com->handle);
-    com->handle = 0;
+	if (index < 0 || index >= noDevices)
+		return;
+	COMDevice *com = &comDevices[index];
+	if (!com->handle)
+		return;
+	CloseHandle(com->handle);
+	com->handle = 0;
 }
 
 void comCloseAll()
 {
-    for (int i = 0; i < noDevices; i++)
-        comClose(i);
+	for (int i = 0; i < noDevices; i++)
+		comClose(i);
 }
 
 /*****************************************************************************/
-int comWrite(int index, const char * buffer, size_t len)
+int comWrite(int index, const char *buffer, size_t len)
 {
-    if (index < 0 || index >= noDevices)
-        return 0;
-    COMDevice * com = &comDevices[index];
-    uint32_t bytes = 0;
-    WriteFile(com->handle, buffer, len, &bytes, NULL);
-    return bytes;
+	if (index < 0 || index >= noDevices)
+		return 0;
+	COMDevice *com = &comDevices[index];
+	uint32_t bytes = 0;
+	WriteFile(com->handle, buffer, len, &bytes, NULL);
+	return bytes;
 }
 
-int comRead(int index, char * buffer, size_t len)
+int comRead(int index, char *buffer, size_t len)
 {
-    if (index < 0 || index >= noDevices)
-        return 0;
-    COMDevice * com = &comDevices[index];
-    uint32_t bytes = 0;
-    ReadFile(com->handle, buffer, len, &bytes, NULL);
-    return bytes;
+	if (index < 0 || index >= noDevices)
+		return 0;
+	COMDevice *com = &comDevices[index];
+	uint32_t bytes = 0;
+	ReadFile(com->handle, buffer, len, &bytes, NULL);
+	return bytes;
 }
 
 /*****************************************************************************/
-const char * findPattern(const char * string, const char * pattern, int * value)
+const char *findPattern(const char *string, const char *pattern, int *value)
 {
-    char c, n = 0;
-    const char * sp = string;
-    const char * pp = pattern;
+	char c, n = 0;
+	const char *sp = string;
+	const char *pp = pattern;
 // Check for the string pattern
-    while (1) {
-        c = *sp ++;
-        if (c == '\0') {
-            if (*pp == '?') break;
-            if (*sp == '\0') break;
-            n = 0;
-            pp = pattern;
-        }else{
-            if (*pp == '?') {
-            // Expect a digit
-                if (c >= '0' && c <= '9') {
-                    n = n * 10 + (c - '0');
-                    if (*pp ++ == '\0') break;
-                }else{
-                    n = 0;
-                    pp = comPtn;
-                }
-            }else{
-            // Expect a character
-                if (c == *pp) {
-                    if (*pp ++ == '\0') break;
-                }else{
-                    n = 0;
-                    pp = comPtn;
-                }
-            }
-        }
-    }
+	while (1) {
+		c = *sp ++;
+		if (c == '\0') {
+			if (*pp == '?') break;
+			if (*sp == '\0') break;
+			n = 0;
+			pp = pattern;
+		} else {
+			if (*pp == '?') {
+				// Expect a digit
+				if (c >= '0' && c <= '9') {
+					n = n * 10 + (c - '0');
+					if (*pp ++ == '\0') break;
+				} else {
+					n = 0;
+					pp = comPtn;
+				}
+			} else {
+				// Expect a character
+				if (c == *pp) {
+					if (*pp ++ == '\0') break;
+				} else {
+					n = 0;
+					pp = comPtn;
+				}
+			}
+		}
+	}
 // Return the value
-    * value = n;
-    return sp;
+	* value = n;
+	return sp;
 }
 
 #endif // _WIN32
