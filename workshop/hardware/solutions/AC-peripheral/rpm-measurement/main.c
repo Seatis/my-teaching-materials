bcccda2ebaf0c1e717d5065c53b2df20981f674c
koincidencia <kazykazy91@gmail.com>
2017-06-02 16:42:03 +0200

    fix(hw-AC): style

--- workshop/hardware/solutions/AC-peripheral/rpm-measurement/main.c
+++ workshop/hardware/solutions/AC-peripheral/rpm-measurement/main.c
@@ -35,9 +35,9 @@ int main(void)
 	// Setting up STDIO input and output buffer
 	// You don't have to understand this!
 	//----- START OF STDIO IO BUFFER SETUP
-	FILE UART_output = FDEV_SETUP_STREAM((void*)UART_send_character, NULL, _FDEV_SETUP_WRITE);
+	FILE UART_output = FDEV_SETUP_STREAM((void *)UART_send_character, NULL, _FDEV_SETUP_WRITE);
 	stdout = &UART_output;
-	FILE UART_input = FDEV_SETUP_STREAM(NULL, (void*)UART_get_character, _FDEV_SETUP_READ);
+	FILE UART_input = FDEV_SETUP_STREAM(NULL, (void *)UART_get_character, _FDEV_SETUP_READ);
 	stdin = &UART_input;
 	//----- END OF STDIO IO BUFFER SETUP
 
