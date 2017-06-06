3ffd1151ea899311e6731bfa467a408e540d0def
Veréb Szabolcs <kazykazy91@gmail.com>
2017-06-06 22:22:12 +0200

    fix(hw-I2C): volatile

--- workshop/hardware/I2C-communication/workshop/AtmelStudio/TC74_DRIVER/UART_driver.h
+++ workshop/hardware/I2C-communication/workshop/AtmelStudio/TC74_DRIVER/UART_driver.h
@@ -7,11 +7,11 @@
 
 volatile struct circular_buffer 7
 {
-	char buffer[RX_CIRC_BUFF_LEN];
-	char *head;
-	char *tail;
-	char *write_ptr;
-	char *read_ptr;
+	volatile char buffer[RX_CIRC_BUFF_LEN];
+	volatile char *head;
+	volatile char *tail;
+	volatile char *write_ptr;
+	volatile char *read_ptr;
 } rx_buffer;
 
 void UART_init();
