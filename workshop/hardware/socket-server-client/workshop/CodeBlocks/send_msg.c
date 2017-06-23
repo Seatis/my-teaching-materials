df1f6a30e76663261216919e72da9e6a0b48b4f3
Lollypopp <zsolt.farkas@mechatromotive.com>
2017-06-23 23:55:03 +0200

    fix(hw-socket-client): description

--- workshop/hardware/socket-server-client/workshop/CodeBlocks/send_msg.c
+++ workshop/hardware/socket-server-client/workshop/CodeBlocks/send_msg.c
@@ -3,15 +3,7 @@
 #include <stdlib.h>
 #include <winsock2.h>
 
-// Create Server and Client Applications based on the sample code written by egdice
-// that send and receive numeric messages. The messages should include a clientID
-// a step ID and a timestemp. Or better yet; Think about this: What kind of information do You need
-// to send in a message so that:
-//   - Each message is definitely unique
-//   - Each can be identified eg.: to which client it belongs
-//   - Each tells a certain step in an amoeba game so that it's quite certain what happened :)
-// Make it so, that the server application prints them to the onsole and logs then into a file.
-// The file should have a name that makes it unique and ties it to a specific starting date & time.
+// Create Server and Client Applications that send and receive messages. 
 
 int main()
 {
