1b6d81b93676c88df0a16cceded542b2b2903190
Levente Belteky <belteky.levente@gmail.com>
2017-07-26 17:18:03 +0200

    fix(expr-ctrl-flow): hello-others exercise
    create the c exercise file
    rename the python file

---
+++ workshop/expressions-and-control-flow/hello-others/hello_others.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+main()
+{
+  // Greet 3 of your classmates with this program, in three separate lines
+  // like:
+  //
+  // Hello, Esther!
+  // Hello, Mary!
+  // Hello, Joe!
+}
