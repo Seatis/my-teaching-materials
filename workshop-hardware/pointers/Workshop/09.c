c9bba594f2a4cdbc5118cb71d253db35483904e1
koincidencia <kazykazy91@gmail.com>
2017-08-30 16:07:40 +0200

    fix(hw-pointers): function name

--- workshop-hardware/pointers/Workshop/09.c
+++ workshop-hardware/pointers/Workshop/09.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-void safeUnion(int *a, int l_a, int *b, int l_b, int *result, int l_res) {
+void safe_union(int *a, int l_a, int *b, int l_b, int *result, int l_res) {
     //TODO:
     // Implement this function to create the union of a and b into the array result!
     return;
@@ -10,7 +10,7 @@ int main(int argc, char** argv) {
     int a[3] = {1,2,3};
     int b[3] = {4,5,6};
     int c[6];
-    safeUnion(a,3,b,3,c,6);
+    safe_union(a,3,b,3,c,6);
     for (int i = 0; i<6; i++) {
         printf("%d\n", c[i]);
     }
