22854ded1edda6aa3fcdc007df3892f3990c5c10
Gyorgy Gabor <gyorgyg@gmail.com>
2017-09-03 00:35:10 +0200

    fix(java exercises)fix filenames

---
+++ workshop/data-structures/lists/appendletter/AppendLetter.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class AppendLetter{
+  public static void main(String... args){
+    ArrayList<String> far = new ArrayList<String>(Arrays.asList("kuty", "macsk", "kacs", "rók", "halacsk"));
+    // Add "a" to every string in the far list.
+
+    System.out.println(far);
+  }
+}
---
+++ workshop/data-structures/lists/solarsystem/SolarSystem.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class SolarSystem{
+  public static void main(String... args){
+    ArrayList<String> planetList = new ArrayList<String>(Arrays.asList("Mercury","Venus","Earth","Mars","Jupiter","Uranus","Neptune");
+
+    // Saturn is missing from the planetList
+    // Insert it into the correct position
+
+    System.out.println(planetList);
+  }
+}
