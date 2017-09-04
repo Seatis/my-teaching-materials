9e01cdf4b6e266e8573468ad7aac26af9174978f
herrnorbert <nkobli@yahoo.com>
2017-09-04 16:59:37 +0200

    feat(func-n-arrays): add c# function tasks

---
+++ workshop/functions-and-arrays/exercises/functions/printer/Printer.cs
@@ -0,0 +1,12 @@
+//  Create the usual class wrapper
+//  and main method on your own.
+
+// - Create a function called `printer`
+//   which prints the input String parameters
+// - It can have any number of parameters
+
+// Examples
+// printer("first")
+// printer("first", "second")
+// printer("first", "second", "third", "fourh")
+// ...
