ac3a4236a419c35ed575f286636f41e0e67bf4a5
herrnorbert <nkobli@yahoo.com>
2017-09-05 12:02:58 +0200

    feat(exercises): add .cs files

---
+++ workshop/expressions-and-control-flow/hello-others/HelloOthers.cs
@@ -0,0 +1,10 @@
+public class HelloOthers {
+  public static void main(String[] args) {
+    // Greet 3 of your classmates with this program, in three separate lines
+    // like:
+    //
+    // Hello, Esther!
+    // Hello, Mary!
+    // Hello, Joe!
+  }
+}
---
+++ workshop/expressions-and-control-flow/hello-others/solutions/HelloOthers.cs
@@ -0,0 +1,7 @@
+public class HelloOthers {
+  public static void main(String[] args) {
+    System.out.println("Hello, Sarah!");
+    System.out.println("Hello, John!");
+    System.out.println("Hello, Dory!");
+  }
+}
