9eb76aa6aa81705f90b292e2f0e8ff0ceabe24d4
leventekobor94 <levente.kobor94@gmail.com>
2017-09-08 11:45:34 +0200

    fix(expressions-and-control-flow): indents

--- workshop/expressions-and-control-flow/variable-mutation/VariableMutation.cs
+++ workshop/expressions-and-control-flow/variable-mutation/VariableMutation.cs
@@ -23,37 +23,37 @@ namespace GreenFox
         Console.WriteLine(c);
 
         int d = 125;
-    	// please divide by 5 d's value
+        // please divide by 5 d's value
 
         Console.WriteLine(d);
 
-    	int e = 8;
-    	// please cube of e's value
+        int e = 8;
+        // please cube of e's value
 
         Console.WriteLine(e);
 
-    	int f1 = 123;
-      	int f2 = 345;
-      	// tell if f1 is bigger than f2 (print as a boolean)
+        int f1 = 123;
+        int f2 = 345;
+        // tell if f1 is bigger than f2 (print as a boolean)
 
-    	int g1 = 350;
-      	int g2 = 200;
-      	// tell if the double of g2 is bigger than g1 (print as a boolean)
+        int g1 = 350;
+        int g2 = 200;
+        // tell if the double of g2 is bigger than g1 (print as a boolean)
 
-    	int h = 1357988018575474;
-    	// tell if it has 11 as a divisor (print as a boolean)
+        int h = 1357988018575474;
+        // tell if it has 11 as a divisor (print as a boolean)
 
-    	int i1 = 10;
-    	int i2 = 3;
-      	// tell if i1 is higher than i2 squared and smaller than i2 cubed (print as a boolean)
+        int i1 = 10;
+        int i2 = 3;
+        // tell if i1 is higher than i2 squared and smaller than i2 cubed (print as a boolean)
 
-    	int j = 1521;
-      	// tell if j is dividable by 3 or 5 (print as a boolean)
+        int j = 1521;
+        // tell if j is dividable by 3 or 5 (print as a boolean)
 
-    	string k = "Apple";
-      	//fill the k variable with its cotnent 4 times
+        string k = "Apple";
+        //fill the k variable with its cotnent 4 times
 
-    	Console.WriteLine(k);
+        Console.WriteLine(k);
         }
     }
 }
