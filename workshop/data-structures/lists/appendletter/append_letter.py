6356f0ddbe5e64d35575a091c67aa2573c11ee83
Gábor György <gyorgyg@gmail.com>
2017-09-12 23:32:43 +0200

    fix(append_letter.py) fix typo

--- workshop/data-structures/lists/appendletter/append_letter.py
+++ workshop/data-structures/lists/appendletter/append_letter.py
@@ -1,5 +1,5 @@
 # Create a function called 'create_new_verbs()' which takes a list of verbs and a string as parameters
-# The string would be a preverb
+# The string shouldf be a preverb
 # The function appends every verb to the preverb and returns the list of the new verbs
 
 
