05b92ae87c0fa092f882cf83035eff38df88e05f
Gábor György <gyorgyg@gmail.com>
2017-09-12 23:28:16 +0200

    fix(candyshop.py): fix typo

--- workshop/data-structures/lists/candyshop/candyshop.py
+++ workshop/data-structures/lists/candyshop/candyshop.py
@@ -1,4 +1,4 @@
-shop_items = ["Cupcake", 2, "Brownie", false]
+shop_items = ["Cupcake", 2, "Brownie", False]
 
 # Accidentally we added "2" and "false" to the list. 
 # Your task is to change from "2" to "Croissant" and change from "false" to "Ice cream"
