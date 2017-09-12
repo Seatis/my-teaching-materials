db9bdff15dfa753e505a4519f47144732d0ee921
Gábor György <gyorgyg@gmail.com>
2017-09-12 23:08:23 +0200

    style(matchmaking.py): add spaces in the lists

--- workshop/data-structures/lists/matchmaking/matchmaking.py
+++ workshop/data-structures/lists/matchmaking/matchmaking.py
@@ -1,8 +1,8 @@
 # Join the two lists by matching one girl with one boy in the order list
 # Exepected output: ["Eve", "Joe", "Ashley", "Fred"...]
 
-girls = ["Eve","Ashley","Bözsi","Kat","Jane"]
-boys = ["Joe","Fred","Béla","Todd","Neef","Jeff"]
+girls = ["Eve", "Ashley", "Bözsi", "Kat", "Jane"]
+boys = ["Joe", "Fred", "Béla", "Todd", "Neef", "Jeff"]
 order = []
 
 print(order)
