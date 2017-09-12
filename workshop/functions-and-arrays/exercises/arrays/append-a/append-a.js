7fe4f56ff8a28e6cd817bfceff74e635f16bd800
Tibor Szász <tszasz@gmail.com>
2017-09-12 16:16:54 +0200

    fix(append-a): fix missing char

--- workshop/functions-and-arrays/exercises/arrays/append-a/append-a.js
+++ workshop/functions-and-arrays/exercises/arrays/append-a/append-a.js
@@ -1,5 +1,5 @@
 'use strict';
 // - Create a variable named `nimals`
-//   with the following content: `["kuty", "macs", "cic"]`
+//   with the following content: `["kuty", "macsk", "cic"]`
 // - Add all elements an `"a"` at the end
 // - try to use built in functions instead of loops
