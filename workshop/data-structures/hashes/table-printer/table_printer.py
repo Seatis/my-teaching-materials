6ec8df977ebdd07cc6839b59f4c2bc92bb0b575c
Tibor Szász <tszasz@gmail.com>
2017-09-13 22:27:33 +0200

    fix(table-printer): simplified data structure

--- workshop/data-structures/hashes/table-printer/table_printer.py
+++ workshop/data-structures/hashes/table-printer/table_printer.py
@@ -10,15 +10,16 @@
 # | mint_leaves        | No            | -        |
 # +--------------------+---------------+----------+
 #
+# OPTIONAL:
 # The frist columns should be automatically as wide as the longest key
 
 ingredients = [
-	{ 'vodka': 1, 'needs_cooling': True }
-	{ 'coffee_liqueur': 0, 'needs_cooling': True }
-	{ 'fresh_cream': 1, 'needs_cooling': True }
-	{ 'captain_morgan_rum': 2, 'needs_cooling': True }
-	{ 'mint_leaves': 0, 'needs_cooling': False }
-	{ 'sugar': 100, 'needs_cooling': False }
-	{ 'lime juice': 10, 'needs_cooling': True }
-	{ 'soda': 100, 'needs_cooling': True }
+	{ "name": "vodka", "in_stock": 1, "needs_cooling": True },
+	{ "name": "coffee_liqueur", "in_stock": 0, "needs_cooling": True },
+	{ "name": "fresh_cream", "in_stock": 1, "needs_cooling": True },
+	{ "name": "captain_morgan_rum", "in_stock": 2, "needs_cooling": True },
+	{ "name": "mint_leaves", "in_stock": 0, "needs_cooling": False },
+	{ "name": "sugar", "in_stock": 0, "needs_cooling": False },
+	{ "name": "lime juice", "in_stock": 0, "needs_cooling": True },
+	{ "name": "soda", "in_stock": 0, "needs_cooling": True }
 ]
