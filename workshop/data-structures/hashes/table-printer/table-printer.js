0c4d0741626b5165133fa319bac620621ed9e1ab
Tibor Szász <tszasz@gmail.com>
2017-09-13 22:28:31 +0200

    fix(table-print): simplified data structure

--- workshop/data-structures/hashes/table-printer/table-printer.js
+++ workshop/data-structures/hashes/table-printer/table-printer.js
@@ -11,15 +11,16 @@
 // | mint_leaves        | No            | -        |
 // +--------------------+---------------+----------+
 //
+// OPTIONAL
 // The frist columns should be automatically as wide as the longest key
 
-var ingredients = [
-	{ 'vodka': 1, 'needs_cooling': true },
-	{ 'coffee_liqueur': 0, 'needs_cooling': true },
-	{ 'fresh_cream': 1, 'needs_cooling': true },
-	{ 'captain_morgan_rum': 2, 'needs_cooling': true },
-	{ 'mint_leaves': 0, 'needs_cooling': false },
-	{ 'sugar': 100, 'needs_cooling': false },
-	{ 'lime juice': 10, 'needs_cooling': true },
-	{ 'soda': 100, 'needs_cooling': true }
+const ingredients = [
+	{ "name": "vodka", "in_stock": 1, "needs_cooling": True },
+	{ "name": "coffee_liqueur", "in_stock": 0, "needs_cooling": True },
+	{ "name": "fresh_cream", "in_stock": 1, "needs_cooling": True },
+	{ "name": "captain_morgan_rum", "in_stock": 2, "needs_cooling": True },
+	{ "name": "mint_leaves", "in_stock": 0, "needs_cooling": False },
+	{ "name": "sugar", "in_stock": 0, "needs_cooling": False },
+	{ "name": "lime juice", "in_stock": 0, "needs_cooling": True },
+	{ "name": "soda", "in_stock": 0, "needs_cooling": True }
 ]
