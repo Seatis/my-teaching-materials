9d325820e3bc0492abb2ad957988a02e3faa3306
aze <adam.gyulavari@lab.coop>
2017-09-15 12:17:16 +0200

    fix(data-structures) he will never ex syntax closes #200

--- workshop/data-structures/strings/hewillnever/HeWillNever.java
+++ workshop/data-structures/strings/hewillnever/HeWillNever.java
@@ -7,14 +7,14 @@ public class HeWillNever {
 
     Map<String, String> map = new HashMap<String, String>();
 
-    hmap.put(7, "run around and desert you");
-    hmap.put(50, "tell a lie and hurt you");
-    hmap.put(49, "make you cry,");
-    hmap.put(2, "let you down");
-    hmap.put(12, "give you up,");
-    hmap.put(1, "Never gonna");
-    hmap.put(11, "\n");
-    hmap.put(3, "say goodbye");
+    map.put(7, "run around and desert you");
+    map.put(50, "tell a lie and hurt you");
+    map.put(49, "make you cry,");
+    map.put(2, "let you down");
+    map.put(12, "give you up,");
+    map.put(1, "Never gonna");
+    map.put(11, "\n");
+    map.put(3, "say goodbye");
 
     // Things are a little bit messed up
     // Your job is to decode the notSoCrypticMessage by using the hashmap as a look up table
