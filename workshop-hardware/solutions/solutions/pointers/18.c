a0f9dfb6d07dde861119a3db959ddbe0921d4e6d
samsmith94 <sunikovacs@gmail.com>
2017-09-23 21:59:48 +0200

    fix(hw-pointers-refactor): fix README links

--- workshop-hardware/solutions/solutions/pointers/18.c
+++ workshop-hardware/solutions/solutions/pointers/18.c
@@ -1,50 +1,36 @@
 #include <stdio.h>
-#define MAX_CHARS 255 // Maximum characters allowed
 
+int first_word(char *string, char *first);
 
 int main()
 {
-    //TODO: write a function which finds the maximum occurrence of a character in a given string.
-    //return with the most frequent character
-    //the number of occurrence should be an output parameter
+    //TODO: write a function, which extracts the first word of a given string.
+    //return with the word's length
 
-    char str[] = "e is the most frequent element.";
+    char str[] = "Extracting the first word.";
+    char first[50];
 
-    int occurrence = 0;
-    int character = freq(str, &occurrence);
-    printf("Maximum occurring character is %c.\n", character);
-    printf("Number of occurrence: %d.", occurrence);
+    int size_of_first_word = first_word(str, first);
+
+    int i;
+    for (i = 0; i<size_of_first_word; i++) {
+        printf("%c", first[i]);
+    }
 
     return 0;
 }
 
-int freq(char *string, int *occurrence)
+int first_word(char *string, char *first)
 {
-    int freq[MAX_CHARS]; // Store frequency of each character
-    int ascii;
+    int pos = 0;
 
-    //Initializes frequency of all characters to 0
     int i;
-    for(i=0; i<MAX_CHARS; i++) {
-        freq[i] = 0;
+    for (i = 0; string[i]!=' '; i++) {
+        pos++;
     }
 
-    //Finds frequency of each characters
-    i=0;
-    while(string[i] != '\0') {
-        ascii = (int)string[i];
-        freq[ascii]++;
-        i++;
+    for (i = 0; i < pos; i++) {
+        first[i] = string[i];
     }
-
-    //Finds maximum frequency
-    int max = 0;
-    for(i=0; i<MAX_CHARS; i++) {
-        if(freq[i] > freq[max])
-            max = i;
-    }
-
-    *occurrence = freq[max];
-
-    return max;
+    return pos;
 }
