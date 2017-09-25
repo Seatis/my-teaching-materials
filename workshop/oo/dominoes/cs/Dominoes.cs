37a58c20f928251616bf5d27b5059681bb998b3a
Suviii <istvan.suvoltos@gmail.com>
2017-09-25 14:04:48 +0200

    fix(dominoes): check styleguide

--- workshop/oo/dominoes/cs/Dominoes.cs
+++ workshop/oo/dominoes/cs/Dominoes.cs
@@ -18,12 +18,12 @@ namespace Domino
         public static List<Domino> InitializeDominoes()
         {
             var dominoes = new List<Domino>();
-            dominoes.add(new Domino(5, 2));
-            dominoes.add(new Domino(4, 6));
-            dominoes.add(new Domino(1, 5));
-            dominoes.add(new Domino(6, 7));
-            dominoes.add(new Domino(2, 4));
-            dominoes.add(new Domino(7, 1));
+            dominoes.Add(new Domino(5, 2));
+            dominoes.Add(new Domino(4, 6));
+            dominoes.Add(new Domino(1, 5));
+            dominoes.Add(new Domino(6, 7));
+            dominoes.Add(new Domino(2, 4));
+            dominoes.Add(new Domino(7, 1));
             return dominoes;
         }
     }
