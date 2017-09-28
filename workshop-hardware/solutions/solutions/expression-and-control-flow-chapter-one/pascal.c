f4c5ed331d8d7d3b107c6667167e18da461055a2
Lollypopp <zsolt.farkas@mechatromotive.com>
2017-09-28 11:53:53 +0200

    fix(hw-expression-and-control-flow-chapter-one): Styleguide

--- workshop-hardware/solutions/solutions/expression-and-control-flow-chapter-one/pascal.c
+++ workshop-hardware/solutions/solutions/expression-and-control-flow-chapter-one/pascal.c
@@ -9,14 +9,14 @@ int main()
     int k = 0;
     int space = 0;
 
-    for(i=1; i<=rows; ++i, k=0)
+    for(i = 1; i <= rows; ++i, k = 0)
     {
-        for(space=1; space<=rows-i; ++space)
+        for(space = 1; space <= rows - i; ++space)
         {
             printf("  ");
         }
 
-        while(k != 2*i-1)
+        while(k != 2 * i - 1)
         {
             printf("* ");
             ++k;
@@ -25,15 +25,15 @@ int main()
         printf("\n");
     }
 
-    for(i=rows; i>=1; --i)
+    for(i = rows; i >= 1; --i)
     {
-        for(space=0; space < rows-i; ++space)
+        for(space = 0; space < rows - i; ++space)
             printf("  ");
 
-        for(j=i; j <= 2*i-1; ++j)
+        for(j = i; j <= 2 * i - 1; ++j)
             printf("* ");
 
-        for(j=0; j < i-1; ++j)
+        for(j = 0; j < i - 1; ++j)
             printf("* ");
 
         printf("\n");
