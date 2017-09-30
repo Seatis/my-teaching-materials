f7ec6e75e0908aff1def3700c5cd970b868b2f39
samsmith94 <sunikovacs@gmail.com>
2017-09-30 16:04:18 +0200

    fix(hw-control-flow-refactor): modify 14.c todo description in chapter 2

--- workshop-hardware/expressions-and-control-flow-chapter-two/Workshop/14.c
+++ workshop-hardware/expressions-and-control-flow-chapter-two/Workshop/14.c
@@ -13,6 +13,14 @@ and the output should be:
 1-20Ft
 */
 
+/*
+Take care of the rounding (when giving back 5 coins):
+1, 2 - 0 down
+3, 4 - 5 up
+6, 7 - 5 down
+8, 9 - 0 up
+*/
+
 int main()
 {
     int amount_of_money = 57545;
